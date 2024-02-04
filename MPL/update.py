8a90b02d1db9723b21ace8726383a6fda8fb7983
buonviri <andy@buonviri.com>
2024-02-04 09:46:50 -0500

    added sorted list
    now retrieves info from each part file and generates a sorted list of all parts

--- MPL/update.py
+++ MPL/update.py
@@ -1,4 +1,5 @@
 import os
+import pprint
 
 # project name, pick whatever you want, do not ever change it!
 project = 'ECPL'
@@ -20,6 +21,7 @@ alt_extension = '.TXT'
 name_length = len(prefix) + len(anchor) + 1 + len(extension)
 
 # this list of categories contains tuples with category letter and category name
+# the order of parts in the html file will match the order in this list
 categories =[
     ('A','Assemblies',),
     ('B','Sub-Assemblies',),
@@ -34,6 +36,11 @@ categories =[
 # blank log
 logtext = ''
 
+# dictionary for exporting
+info = {}
+
+# list of all parts for generating html in correct order
+allparts = []
 
 # logging function
 def log(s):
@@ -42,7 +49,7 @@ def log(s):
 
 
 # add category
-def add(letter, name):
+def AddCategory(letter, name):
     os.mkdir(letter)  # add folder
     log('Added folder: ' + cat_letter + ' (' + cat_name + ')')
     current_folder = cat_letter  # initialize
@@ -56,19 +63,38 @@ def add(letter, name):
     log('Added file: ' + anchor_filename)
 
 
+# get info
+def GetInfo(fullfilename):
+    with open (fullfilename, 'r') as f:
+        lines = [line.strip() for line in f]  # generates a list of the lines without whitespace
+    return lines
+
+
 # iterate over all the categories
 for c in categories:
+    parts = []  # blank list for storing part numbers to be sorted later
     cat_letter = c[0]
     cat_name = c[1]
+    info[cat_letter] = {}  # add category to dict
+    info[cat_letter]['name'] = cat_name  # add name to dict
     if not os.path.isdir(cat_letter):  # check if letter is missing as folder
-        add(cat_letter, cat_name)  # add folder
+        AddCategory(cat_letter, cat_name)  # add folder
     else:
         for dirname, dirnames, filenames in os.walk(cat_letter):  # get info from category
             for filename in filenames:
                 # check if length and extension are correct
                 if len(filename) == name_length and filename[-len(extension):] in [extension, alt_extension]:
-                    log('Found: ' + filename)
+                    part = filename[0:-len(extension)]  # remove extension
+                    info[cat_letter][part] = GetInfo(os.path.join(dirname,filename))  # add info from file to dict
+                    parts.append(part)  # add part to temporary list
+    allparts = allparts + sorted(parts)
 
-# update log file
+# write dict
+with open (project + '.dict', 'w') as f:
+    f.write(pprint.pformat(info, indent=2))
+
+# append log file
 with open (project + '.log', 'a') as f:
     f.write(logtext)
+
+print(allparts)
