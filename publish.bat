6b6fdd29f4fb8c7d35f6c694cf7fd45d71892847
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-03-27 10:33:24 -0400

    added batch file to publish to patrick's repo

---
+++ publish.bat
@@ -0,0 +1,3 @@
+xcopy C:\EdgeCortix\HJS\9182B\9182B.py C:\EdgeCortix\hw-benchmarking\9182B\9182B.py /y
+xcopy C:\EdgeCortix\HJS\statlog\statlog.py C:\EdgeCortix\hw-benchmarking\statlog\statlog.py /y
+xcopy C:\EdgeCortix\HJS\csvmerge\csvmerge.py C:\EdgeCortix\hw-benchmarking\csvmerge\csvmerge.py /y
