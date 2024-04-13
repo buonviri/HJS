af273e317b14c19f7edc56303d3b9aeb6ad81134
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-04-13 19:03:23 -0400

    moved timeout to global, added desktop icons

--- u22/gnome.sh
+++ u22/gnome.sh
@@ -12,8 +12,6 @@ then
     # gsettings get org.gnome.desktop.background picture-uri
     gsettings set org.gnome.desktop.background picture-uri-dark file:///home/ec/HJS/u22/black.png
     gsettings get org.gnome.desktop.background picture-uri-dark
-    gsettings set org.gnome.desktop.session idle-delay 900
-    gsettings get org.gnome.desktop.session idle-delay
 else
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-light'
     gsettings get org.gnome.desktop.interface color-scheme
@@ -21,11 +19,21 @@ else
     gsettings get org.gnome.desktop.background picture-uri
     # gsettings set org.gnome.desktop.background picture-uri-dark file:///home/ec/HJS/u22/white.png
     # gsettings get org.gnome.desktop.background picture-uri-dark
-    gsettings set org.gnome.desktop.session idle-delay 900
-    gsettings get org.gnome.desktop.session idle-delay
 fi
 
+# set screen timeout
+gsettings set org.gnome.desktop.session idle-delay 900
+gsettings get org.gnome.desktop.session idle-delay
+
+# remove trash, note that older versions require .extensions.desktop-icons instead
+gsettings set org.gnome.shell.extensions.ding show-trash false
+gsettings get org.gnome.shell.extensions.ding show-trash
+
+# remove home, note that older versions require .extensions.desktop-icons instead
+gsettings set org.gnome.shell.extensions.ding show-home false
+gsettings get org.gnome.shell.extensions.ding show-home
+
 echo
-echo Setup complete.
+echo Setup complete. Sincerely, HJS
 
 # EOF
