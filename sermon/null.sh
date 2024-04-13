01f79e36743e0a458b523b7057c1eb92f94ee878
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-04-13 18:59:38 -0400

    null opens two windows now

--- sermon/null.sh
+++ sermon/null.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-# start a new window that creates a null modem
+# make sure socat is installed (sudo apt install socat)
+
+# start a new window that creates a null modem, window is otherwise unusable
 gnome-terminal -- sh -c 'socat -d -d pty,raw,echo=0,link="/home/ec/COM5" pty,raw,echo=0,link="/home/ec/COM6"'
+
+# start in same terminal, gets spammy
+# socat -d -d pty,raw,echo=0,link="/home/ec/COM5" pty,raw,echo=0,link="/home/ec/COM6" &
+
+echo Starting 9128B serial monitor...
+cd ~/HJS/sermon
+python3 sm-9182.py
 
 # EOF
