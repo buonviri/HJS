e3973fa358f657424b42ea75d2ff35f50c0aaa3d
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-04-20 22:22:30 -0400

    yaml test to mimic serial.cfg and test.cfg

---
+++ yaml/test.py
@@ -0,0 +1,12 @@
+import os
+import yaml  # requires pip install pyyaml
+
+with open("test.yaml") as f:
+    try:
+        print(yaml.safe_load(f))
+    except:
+        print('YAML Error')
+
+os.system('PAUSE')
+
+# EOF
