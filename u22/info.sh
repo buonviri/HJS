6e2f0be025283603346ccd49dd3ba14c503e57bd
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-04-25 18:59:10 -0400

    added to cpu script

--- u22/info.sh
+++ u22/info.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-lscpu | grep -Po 'Model name:\s+\K.*' | tee ~/mycpu.txt
+sudo dmidecode --string baseboard-manufacturer | tee ~/sys.info
+sudo dmidecode --string baseboard-product-name | tee -a ~/sys.info
+lscpu | grep -Po 'Model name:\s+\K.*' | tee -a ~/sys.info
+lsb_release -d | grep -Po 'Description:\s+\K.*' | tee -a ~/sys.info
+uname -r | tee -a ~/sys.info
 
 # EOF
