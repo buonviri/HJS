9a0057ffa6d341b36686a496497b6efea14bc65a
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-04-26 12:50:59 -0400

    added power profile

--- u22/info.sh
+++ u22/info.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-sudo dmidecode --string baseboard-manufacturer | tee ~/sys.info
+# create new file or empty existing one
+echo -n "" > filename
+
+# append lines with system info
+sudo dmidecode --string baseboard-manufacturer | tee -a ~/sys.info
 sudo dmidecode --string baseboard-product-name | tee -a ~/sys.info
 lscpu | grep -Po 'Model name:\s+\K.*' | tee -a ~/sys.info
 lsb_release -d | grep -Po 'Description:\s+\K.*' | tee -a ~/sys.info
 uname -r | tee -a ~/sys.info
+powerprofilesctl get | tee -a ~/sys.info
 
 # EOF
