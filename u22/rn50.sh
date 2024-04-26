f42dcac7766c883f866afb2c631a394e4d43f7e5
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-04-26 13:48:04 -0400

    added 60x loop

--- u22/rn50.sh
+++ u22/rn50.sh
@@ -5,6 +5,12 @@ cd ~/S1LP/install_mera/ && source start.sh && mera --sakura1_start
 # make sure this was done once:
 # cd ~/S1LP/inference/test/run_resnet50 && python3 deploy.py
 
-cd ~/S1LP/inference/test/run_resnet50 && python run_resnet50.py --target ip --forever --log --inference_freq 3
+cd ~/S1LP/inference/test/run_resnet50
+
+for run in {1..60}; do
+   python run_resnet50.py --target ip --log --max_log 20 --forever --inference_freq 3
+done
+
+cd ~/HJS/u22
 
 # EOF
