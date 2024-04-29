14157f1edfd196eeae93a3e05ead2305b8abe93e
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-04-29 18:03:17 -0400

    added fast versions

--- statlog/alt.bat
+++ statlog/alt.bat
@@ -1,3 +1,4 @@
 xcopy statlog.py help.py /Y
 xcopy statlog.py zmax.py /Y
+xcopy statlog.py snread-fast.py /Y
 xcopy statlog.py snread.py /Y
--- statlog/alt.sh
+++ statlog/alt.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cp -f statlog.py help.py
 cp -f statlog.py zmax.py
+cp -f statlog.py snread-fast.py
 cp -f statlog.py snread.py
