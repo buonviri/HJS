c072b92a5b82bc9552569bee2c2a863d6fd9bc2b
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-04-29 15:22:14 -0400

    Create lsusb.py

---
+++ u22/lsusb.py
@@ -0,0 +1,30 @@
+from subprocess import Popen, PIPE
+
+process = Popen(['lsusb'], stdout=PIPE, stderr=PIPE)
+stdout, stderr = process.communicate()
+lsusb = stdout.decode("utf-8")
+
+# print(lsusb)
+
+devtypes = {'hub': [], 'mouse': [], 'keyboard': [], '802.11': []}
+misc = []
+
+devices = lsusb.split('\n')
+for device in devices:
+    s = device.strip()
+    print(s)
+    for devtype in devtypes:
+        # print(devtype + ' --- ' + s)
+        if devtype in s.lower():
+            devtypes[devtype].append(s)
+            s = ''
+    if len(s) > 0:
+        misc.append(s)
+
+for devtype in devtypes:
+    print(devtype + ': ')
+    for line in devtypes[devtype]:
+        print('  ' + line)
+if len(misc) > 0:
+    print(misc)
+
