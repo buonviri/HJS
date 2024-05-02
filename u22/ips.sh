76a9ece7d5cbc3c05ba2d94f0e177431d8985944
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-05-02 15:04:17 -0400

    fixed bad file name, added u20 notes

--- u22/ips.sh
+++ u22/ips.sh
@@ -11,14 +11,17 @@ cd ~/S1LP/install_mera/ && source start.sh && mera --sakura1_start
 # change to test folder
 cd ~/S1LP/inference/
 
-# store system info (u20, skips bios-revision and powerprofilescfg)
+# store system info
 echo -n "" > $hexstamp.info
 sudo dmidecode --string baseboard-manufacturer | tee -a $hexstamp.info
 sudo dmidecode --string baseboard-product-name | tee -a $hexstamp.info
-sudo dmidecode --string bios-version | tee -a ~/sys.info
+# u20 - skip bios-revision
+sudo dmidecode --string bios-version | tee -a $hexstamp.info
 lscpu | grep -Po 'Model name:\s+\K.*' | tee -a $hexstamp.info
 lsb_release -d | grep -Po 'Description:\s+\K.*' | tee -a $hexstamp.info
 uname -r | tee -a $hexstamp.info
+# u20 - skip powerprofilescfg
+# need consistent way to read S1LP SN
 
 # run
 python run_models.py --csv_name $hexstamp
