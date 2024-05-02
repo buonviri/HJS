6b00197710de1050ccfb08445c2b290b78d9370c
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-05-02 14:40:29 -0400

    removed old scripts, added new ips

--- u22/ips.sh
+++ u22/ips.sh
@@ -8,26 +8,20 @@ echo Filename: $hexstamp
 # start card
 cd ~/S1LP/install_mera/ && source start.sh && mera --sakura1_start
 
-# make sure this was done once:
-# cd ~/S1LP/inference/test/run_resnet50 && python3 deploy.py
-
 # change to test folder
-cd ~/S1LP/inference/test/run_resnet50
+cd ~/S1LP/inference/
 
-# store system info
+# store system info (u20, skips bios-revision and powerprofilescfg)
 echo -n "" > $hexstamp.info
 sudo dmidecode --string baseboard-manufacturer | tee -a $hexstamp.info
 sudo dmidecode --string baseboard-product-name | tee -a $hexstamp.info
+sudo dmidecode --string bios-version | tee -a ~/sys.info
 lscpu | grep -Po 'Model name:\s+\K.*' | tee -a $hexstamp.info
 lsb_release -d | grep -Po 'Description:\s+\K.*' | tee -a $hexstamp.info
 uname -r | tee -a $hexstamp.info
-powerprofilesctl get | tee -a $hexstamp.info
 
-# run loop, pass timestamp as filename
-for run in {1..60}; do
-   python run_resnet50.py --target ip --log --csv_name $hexstamp --max_log 20 --forever --inference_freq 3
-done
-# 60 loops of 60 seconds: 1..60 in for loop, max_log 20, freq 3
+# run
+python run_models.py --csv_name $hexstamp
 
 # return to my folder
 cd ~/HJS/u22
