f734fc9b6854b281bcf1377026c7e3a8ab34b835
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-05-08 18:46:52 -0400

    publish abcd version instead

--- publish.bat
+++ publish.bat
@@ -1,5 +1,5 @@
 xcopy C:\EdgeCortix\HJS\9182B\9182B.py C:\EdgeCortix\hw-benchmarking\inference\9182B.py /y
 xcopy C:\EdgeCortix\HJS\statlog\statlog.py C:\EdgeCortix\hw-benchmarking\inference\statlog.py /y
-xcopy C:\EdgeCortix\HJS\csvmerge\csvmerge.py C:\EdgeCortix\hw-benchmarking\inference\csvmerge.py /y
+xcopy C:\EdgeCortix\HJS\csvmerge\csv-abcd.py C:\EdgeCortix\hw-benchmarking\inference\csv-abcd.py /y
 xcopy C:\EdgeCortix\HJS\sensorsj\sjscan.py C:\EdgeCortix\hw-benchmarking\inference\sjscan.py /y
 xcopy C:\EdgeCortix\HJS\sensorsj\sjlog.py C:\EdgeCortix\hw-benchmarking\inference\sjlog.py /y
