8decee28c30d5e41a73299bb53cb3a9ba331deb9
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-05-15 20:20:41 -0400

    added comment

--- comma2tab/comma2tab.py
+++ comma2tab/comma2tab.py
@@ -1,5 +1,7 @@
 import pyperclip
 
+# warning: does not check for commas inside quoted strings!
+
 cells = pyperclip.paste()  # get tab data from clipboard
 pyperclip.copy(cells.replace(',', '\t'))  # replace comma with tab and place back on clipboard
 
