930e2a1d26add4b5deac82292fb034fc8730b34e
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-05-21 11:22:38 -0400

    added parameters

--- sensorsj/infotest.py
+++ sensorsj/infotest.py
@@ -1,9 +1,27 @@
 from subprocess import Popen, PIPE
 
-os_command = ['cat', '/sys/devices/virtual/dmi/id/board_vendor']
-process = Popen(os_command, stdout=PIPE, stderr=PIPE)
-stdout, stderr = process.communicate()
-info = stdout.decode("utf-8")
-print(info)
+parameters = {
+    'Board Vendor': ['cat', '/sys/devices/virtual/dmi/id/board_vendor'],
+    'Board Name': ['cat', '/sys/devices/virtual/dmi/id/board_name'],
+}
+
+for label in parameters:
+    os_command = parameters[label]
+    process = Popen(os_command, stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    info = stdout.decode("utf-8")
+    print(label + ': ' + info)
 
 # EOF
+
+"""
+cat /sys/devices/virtual/dmi/id/board_vendor | tee -a ~/sys.info
+cat /sys/devices/virtual/dmi/id/board_name | tee -a ~/sys.info
+cat /sys/devices/virtual/dmi/id/bios_release | tee -a ~/sys.info
+cat /sys/devices/virtual/dmi/id/bios_version | tee -a ~/sys.info
+lscpu | grep -Po 'Model name:\s+\K.*' | tee -a ~/sys.info
+lsb_release -d | grep -Po 'Description:\s+\K.*' | tee -a ~/sys.info
+uname -r | tee -a ~/sys.info
+# doesn't work in 20.04, suppress error
+powerprofilesctl get 2> /dev/null | tee -a ~/sys.info
+"""
