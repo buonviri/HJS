5db88209c93541a4234273e91153b95dfa539f05
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-05-21 10:47:06 -0400

    rewrote using csv instead

--- comma2tab/comma2tab.py
+++ comma2tab/comma2tab.py
@@ -1,8 +1,19 @@
+import csv
 import pyperclip
 
-# warning: does not check for commas inside quoted strings!
 
+def GetTokens(x):  # modified to split on comma
+    return [ '{}'.format(x) for x in list(csv.reader([x], delimiter=',', quotechar='"'))[0] ]
+# end of GetTokens()
+
+
+# start of script
 cells = pyperclip.paste()  # get tab data from clipboard
-pyperclip.copy(cells.replace(',', '\t'))  # replace comma with tab and place back on clipboard
+lines = cells.split('\n')
+out = ''
+for line in lines:
+    newline = GetTokens(line)
+    out = out + '\t'.join(newline) + '\n'
+pyperclip.copy(out)  # place result on clipboard
 
 # EOF
