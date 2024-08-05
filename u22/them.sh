e1f5e62329bc56e6aceae55c41a0575e3294e9cb
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-08-05 12:01:39 -0400

    moved cd line up

--- u22/them.sh
+++ u22/them.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-\cp -v HJS/u22/customer.bash_aliases ~/.bash_aliases
 cd ~/
+\cp -v HJS/u22/customer.bash_aliases ~/.bash_aliases
 
 printf "\n\e[1;35m   Restart terminal to load new aliases.\e[0m\n\n"
 
