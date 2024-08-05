12eaf0a173bd3d8c01e343f45a8d4d5a101666df
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-08-05 12:09:28 -0400

    moved it back so it works from either place

--- u22/us.sh
+++ u22/us.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-cd ~/
+# this works from either the Home folder or the USB top level
 \cp -v HJS/u22/edgecortix.bash_aliases ~/.bash_aliases
+cd ~/
 
 printf "\n\e[1;35m   Restart terminal to load new aliases.\e[0m\n\n"
 
