a68fb43a76cad7e6c5bf99a6733e239fd580b5d6
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-09-06 09:33:37 -0400

    c+d

--- u22/az.sh
+++ u22/az.sh
@@ -8,7 +8,7 @@ cd
 
 # a
 sudo apt install git xsel ntpdate -y
-printf '\n\e[1;35m   Confirm that installation succeeded.\e[0m\n\n'
+# printf '\n\e[1;35m   Confirm that installation succeeded.\e[0m\n\n'
 
 # b: pull if folder exists, else clone
 git -C HJS pull || git clone https://github.com/buonviri/HJS.git
@@ -17,5 +17,10 @@ source alt.sh
 cd
 
 # c
+cd ~/HJS/u22
+source ec.sh
+
+# d
+sudo adduser ec dialout
 
 # EOF
