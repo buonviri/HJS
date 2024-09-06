4637d2d6f6118ea3a3e4fdff4b4f2db6724fd24f
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-09-06 09:28:39 -0400

    started installation script

---
+++ u22/az.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# exit on error
+set -e
+
+# always start in Home
+cd
+
+# a
+sudo apt install git xsel ntpdate -y
+printf '\n\e[1;35m   Confirm that installation succeeded.\e[0m\n\n'
+
+# b: pull if folder exists, else clone
+git -C HJS pull || git clone https://github.com/buonviri/HJS.git
+cd ~/HJS/statlog
+source alt.sh
+cd
+
+# c
+
+# EOF
