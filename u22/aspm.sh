e5c1e81693c2246e611642b87f09b0b1115fcfcb
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-10-25 17:15:06 -0400

    added niceness

--- u22/aspm.sh
+++ u22/aspm.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
 
-sudo dmesg | grep -i aspm | awk '{$1=$1;print}'
-sudo lspci -s 01:00.0 -vvv | grep -i aspm | awk '{$1=$1;print}'
+echo
+
+echo dmesg:
+echo
+sudo dmesg | grep -i --color=always aspm | awk '{$1=$1;print}'
+
+echo
+
+echo lspci:
+echo
+sudo lspci -s 01:00.0 -vvv | grep -i --color=always aspm | awk '{$1=$1;print}'
+
+echo
+
+# end
+
