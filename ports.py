e282077f2c05a387a2cfa80e481f71181252553b
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-10-28 18:54:11 -0400

    Update ports.py

--- ports.py
+++ ports.py
@@ -1,9 +1,15 @@
 # ports.py
 
 import serial.tools.list_ports  # requires pip install pyserial
+import os
 
 for a,b,c in serial.tools.list_ports.comports():
     print()
     print(a)
     print(b)
     print(c)
+
+# assumes windows! FIX IT
+os.system('timeout /t 5')  # wait five seconds then close window or return to prompt
+
+# end
