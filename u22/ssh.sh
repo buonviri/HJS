f43911b14ac4d2bbf4bcf1a490cb5988eaeffb17
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-06 14:05:56 -0500

    added hostname

--- u22/ssh.sh
+++ u22/ssh.sh
@@ -2,11 +2,12 @@
 
 echo
 
+machine=$(hostname)
 sudo apt install openssh-server -y
 sudo systemctl enable ssh
 sudo ufw enable
 sudo ufw allow ssh
-echo Test using Windows: ssh ec@EC-nnnnS
+echo Test using Windows: ssh ec@"$machine"
 
 echo
 
