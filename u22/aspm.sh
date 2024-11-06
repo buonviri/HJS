363298a4db08f218ee19c689eb4393a2a79d6dd8
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-05 22:29:38 -0500

    purple

--- u22/aspm.sh
+++ u22/aspm.sh
@@ -2,13 +2,13 @@
 
 echo
 
-echo dmesg:
+printf "\e[1;35mdmesg:\e[0m\n"
 echo
 sudo dmesg | grep -i --color=always aspm | awk '{$1=$1;print}'
 
 echo
 
-echo lspci:
+printf "\e[1;35mlspci:\e[0m\n"
 echo
 sudo lspci -s 01:00.0 -vvv | grep -i --color=always aspm | awk '{$1=$1;print}'
 
