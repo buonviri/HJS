4ffac26ff024d714070d6d6734058e49f3ee375d
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-07 20:24:56 -0500

    aspm sudo change

--- u22/aspm.sh
+++ u22/aspm.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-echo
+sudo echo
 
-printf "\e[1;35mASPM in dmesg:\e[0m\n"
+printf "\e[1;35m'ASPM' in dmesg:\e[0m\n"
 echo
 sudo dmesg | grep -i --color=always aspm | awk '{$1=$1;print}'
 
 echo
 
-printf "\e[1;35mASPM in lspci:\e[0m\n"
+printf "\e[1;35m'ASPM' in lspci:\e[0m\n"
 echo
 sudo lspci -vvv -d 1fdc:0100 | grep -i --color=always aspm | awk '{$1=$1;print}'
 
