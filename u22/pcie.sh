e84940d6df67db9262ff665b8756ed28b150e327
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-07 20:35:11 -0500

    purple

--- u22/pcie.sh
+++ u22/pcie.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # get bus info
-echo
+sudo echo
 businfo="$(sudo lspci | grep 1fdc:0100 | head -c 7)"  # get first seven characters of the line that contains our ID
 if [ -z "$businfo" ]; then  # check if empty
     printf "  1fdc:0100 not found\n  defaulting to 01:00.0\n\n"  # print error message
     businfo="01:00.0"  # set default
 fi
-echo Displaying info for: $businfo
+printf "\e[1;35mDisplaying info for: $businfo\e[0m\n"
 
 # show card info
 sudo lspci -vvv -s $businfo | grep -E --color=always "Subsystem:|LnkSta:|Region 0:|Region 2:|Region 4:" | awk '{$1=$1;print}'
