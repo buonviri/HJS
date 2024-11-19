0470dbe795db0097853a5b8a7d6b4ef04b281278
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-19 00:07:44 -0500

    sync of bat and sh

--- statlog/alt.bat
+++ statlog/alt.bat
@@ -4,7 +4,11 @@ xcopy statlog.py zmax.py /Y
 xcopy statlog.py snread.py /Y
 xcopy statlog.py snread-fast.py /Y
 xcopy statlog.py S1LP-snread-fast.py /Y
-xcopy statlog.py S2LP-snread-fast.py /Y
+
+xcopy statlog.py hist.py /Y
+xcopy statlog.py xlog.py /Y
+xcopy statlog.py cfg.py /Y
+xcopy statlog.py S2LP-cfg-fast.py /Y
 
 xcopy statlog.py S2LP-fan.a.40-fast.py /Y
 xcopy statlog.py S2LP-fan.a.60-fast.py /Y
--- statlog/alt.sh
+++ statlog/alt.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 cp -f statlog.py help.py
 cp -f statlog.py zmax.py
-cp -f statlog.py snread-fast.py
+
 cp -f statlog.py snread.py
+cp -f statlog.py snread-fast.py
+cp -f statlog.py S1LP-snread-fast.py
+
 cp -f statlog.py hist.py
 cp -f statlog.py xlog.py
 cp -f statlog.py cfg.py
 cp -f statlog.py S2LP-cfg-fast.py
+
+cp -f statlog.py S2LP-fan.a.40-fast.py
+cp -f statlog.py S2LP-fan.a.60-fast.py
+cp -f statlog.py S2LP-fan.a.80-fast.py
+cp -f statlog.py S2LP-fan.a.100-fast.py
+cp -f statlog.py S2LP-fan.py
+cp -f statlog.py S2LP-fan-slow.py
+cp -f statlog.py S2LP-stats-slow.py
