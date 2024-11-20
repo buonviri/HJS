d6152727c556abf497e9d85c0cc64151af827220
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-20 16:44:41 -0500

    added tee statements to create log

--- statlog/CB++.sh
+++ statlog/CB++.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
 echo
-echo "A:"
-python3 statlog.py S2LP-srread.a.0x10100+srwrite.a.0x10100.0+srread.a.0x10100-void
+echo "A:" | tee 'CB.log'
+python3 statlog.py S2LP-srread.a.0x10100+srwrite.a.0x10100.0+srread.a.0x10100-void | tee -a 'CB.log'
 echo "(01 00 00)"
-python3 statlog.py S2LP-pin.AEN_PG1.set+srread.a.0x1001C-void
+python3 statlog.py S2LP-pin.AEN_PG1.set+srread.a.0x1001C-void | tee -a 'CB.log'
 echo "(P203 07)"
-python3 statlog.py S2LP-srwrite.a.0x1001C.4+srread.a.0x1001C-void
+python3 statlog.py S2LP-srwrite.a.0x1001C.4+srread.a.0x1001C-void | tee -a 'CB.log'
 echo "(04 04)"
-python3 statlog.py S2LP-pin.AEN_PG2.set+srread.a.0x1001C-void
+python3 statlog.py S2LP-pin.AEN_PG2.set+srread.a.0x1001C-void | tee -a 'CB.log'
 echo "(P310 04)"
-python3 statlog.py S2LP-srwrite.a.0x1001C.0+srread.a.0x1001C+srwrite.a.0x10100.1+srread.a.0x10100-void
+python3 statlog.py S2LP-srwrite.a.0x1001C.0+srread.a.0x1001C+srwrite.a.0x10100.1+srread.a.0x10100-void | tee -a 'CB.log'
 echo "(00 00 01 01)"
 
 echo
-echo "B:"
-python3 statlog.py S2LP-srread.b.0x10100+srwrite.b.0x10100.0+srread.b.0x10100-void
+echo "B:" | tee -a 'CB.log'
+python3 statlog.py S2LP-srread.b.0x10100+srwrite.b.0x10100.0+srread.b.0x10100-void | tee -a 'CB.log'
 echo "(01 00 00)"
-python3 statlog.py S2LP-pin.BEN_PG1.set+srread.b.0x1001C-void
+python3 statlog.py S2LP-pin.BEN_PG1.set+srread.b.0x1001C-void | tee -a 'CB.log'
 echo "(P009 07)"
-python3 statlog.py S2LP-srwrite.b.0x1001C.4+srread.b.0x1001C-void
+python3 statlog.py S2LP-srwrite.b.0x1001C.4+srread.b.0x1001C-void | tee -a 'CB.log'
 echo "(04 04)"
-python3 statlog.py S2LP-pin.BEN_PG2.set+srread.b.0x1001C-void
+python3 statlog.py S2LP-pin.BEN_PG2.set+srread.b.0x1001C-void | tee -a 'CB.log'
 echo "(P505 04)"
-python3 statlog.py S2LP-srwrite.b.0x1001C.0+srread.b.0x1001C+srwrite.b.0x10100.1+srread.b.0x10100-void
+python3 statlog.py S2LP-srwrite.b.0x1001C.0+srread.b.0x1001C+srwrite.b.0x10100.1+srread.b.0x10100-void | tee -a 'CB.log'
 echo "(00 00 01 01)"
 
 echo
-python3 statlog.py S2LP-stats-void
+python3 statlog.py S2LP-stats-void | tee -a 'CB.log'
 
 # EOF
