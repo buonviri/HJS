61500cfec192d57ca703afc7dce663f8222e3c77
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-20 14:44:10 -0500

    fixed A/B typo

--- statlog/CB++.sh
+++ statlog/CB++.sh
@@ -17,11 +17,11 @@ echo
 echo "B:"
 python3 statlog.py S2LP-srread.b.0x10100+srwrite.b.0x10100.0+srread.b.0x10100-void
 echo "(01 00 00)"
-python3 statlog.py S2LP-pin.AEN_PG1.set+srread.b.0x1001C-void
+python3 statlog.py S2LP-pin.BEN_PG1.set+srread.b.0x1001C-void
 echo "(P009 07)"
 python3 statlog.py S2LP-srwrite.b.0x1001C.4+srread.b.0x1001C-void
 echo "(04 04)"
-python3 statlog.py S2LP-pin.AEN_PG2.set+srread.b.0x1001C-void
+python3 statlog.py S2LP-pin.BEN_PG2.set+srread.b.0x1001C-void
 echo "(P505 04)"
 python3 statlog.py S2LP-srwrite.b.0x1001C.0+srread.b.0x1001C+srwrite.b.0x10100.1+srread.b.0x10100-void
 echo "(00 00 01 01)"
