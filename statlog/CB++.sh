83eff43f4b75bf7bdc395d983d8b80bf9c6e651e
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-20 19:17:08 -0500

    made A and B text longer

--- statlog/CB++.sh
+++ statlog/CB++.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo
-echo "A:" | tee 'CB.log'
+echo "Compute Node A:" | tee 'CB.log'
 python3 statlog.py S2LP-srread.a.0x10100+srwrite.a.0x10100.0+srread.a.0x10100-void | tee -a 'CB.log'
 echo " 01 00 00" | tee -a 'CB.log'
 python3 statlog.py S2LP-pin.AEN_PG1.set+srread.a.0x1001C-void | tee -a 'CB.log'
@@ -14,7 +14,7 @@ python3 statlog.py S2LP-srwrite.a.0x1001C.0+srread.a.0x1001C+srwrite.a.0x10100.1
 echo " 00 00 01 01" | tee -a 'CB.log'
 
 echo
-echo "B:" | tee -a 'CB.log'
+echo "Compute Node B:" | tee -a 'CB.log'
 python3 statlog.py S2LP-srread.b.0x10100+srwrite.b.0x10100.0+srread.b.0x10100-void | tee -a 'CB.log'
 echo " 01 00 00" | tee -a 'CB.log'
 python3 statlog.py S2LP-pin.BEN_PG1.set+srread.b.0x1001C-void | tee -a 'CB.log'
