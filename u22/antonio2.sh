76c907a6466aa6292b8d7c96473bc848bfccdd28
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-22 23:13:48 -0500

    missing newline

--- u22/antonio2.sh
+++ u22/antonio2.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 printf "tar xvzf dna2_self_test.tar.gz\n"
-printf "cd dna2_self_test"
+printf "cd dna2_self_test\n"
 printf "chmod +x ./setup.sh\n"
 printf "./setup.sh\n"
 printf "chmod +x ./run.sh\n"
