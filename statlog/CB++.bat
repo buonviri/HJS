e12312f3a508a0ef0d67b6049eb7cd7eb1a26984
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-26 21:29:18 -0500

    rewrote CB batchfile

--- statlog/CB++.bat
+++ statlog/CB++.bat
@@ -1,69 +1,19 @@
 @echo off
 
-echo A:
-python.exe S2LP-srread.a.0x10100-void.py
-echo ................01
-python.exe S2LP-srwrite.a.0x10100.0-void.py
-echo ................00
-python.exe S2LP-srread.a.0x10100-void.py
-echo ................00
+echo Compute Node A:
+python.exe statlog.py S2LP-srread.a.0x10100+srwrite.a.0x10100.0+srread.a.0x10100-void
+python.exe statlog.py S2LP-pin.AEN_PG1.set+srread.a.0x1001C-void
+python.exe statlog.py S2LP-srwrite.a.0x1001C.4+srread.a.0x1001C-void
+python.exe statlog.py S2LP-pin.AEN_PG2.set+srread.a.0x1001C-void
+python.exe statlog.py S2LP-srwrite.a.0x1001C.0+srread.a.0x1001C+srwrite.a.0x10100.1+srread.a.0x10100-void
 
-python.exe S2LP-pin.AEN_PG1.set-void.py
-echo Pin P203..........
-python.exe S2LP-srread.a.0x1001C-void.py
-echo ................07
+echo Compute Node B:
+python.exe statlog.py S2LP-srread.b.0x10100+srwrite.b.0x10100.0+srread.b.0x10100-void
+python.exe statlog.py S2LP-pin.BEN_PG1.set+srread.b.0x1001C-void
+python.exe statlog.py S2LP-srwrite.b.0x1001C.4+srread.b.0x1001C-void
+python.exe statlog.py S2LP-pin.BEN_PG2.set+srread.b.0x1001C-void
+python.exe statlog.py S2LP-srwrite.b.0x1001C.0+srread.b.0x1001C+srwrite.b.0x10100.1+srread.b.0x10100-void
 
-python.exe S2LP-srwrite.a.0x1001C.4-void.py
-echo ................04
-python.exe S2LP-srread.a.0x1001C-void.py
-echo ................04
+python.exe statlog.py S2LP-stats-void
 
-python.exe S2LP-pin.AEN_PG2.set-void.py
-echo Pin P310..........
-python.exe S2LP-srread.a.0x1001C-void.py
-echo ................04
-
-python.exe S2LP-srwrite.a.0x1001C.0-void.py
-echo ................00
-python.exe S2LP-srread.a.0x1001C-void.py
-echo ................00
-
-python.exe S2LP-srwrite.a.0x10100.1-void.py
-echo ................01
-python.exe S2LP-srread.a.0x10100-void.py
-echo ................01
-
-echo B:
-python.exe S2LP-srread.b.0x10100-void.py
-echo ................01
-python.exe S2LP-srwrite.b.0x10100.0-void.py
-echo ................00
-python.exe S2LP-srread.b.0x10100-void.py
-echo ................00
-
-python.exe S2LP-pin.BEN_PG1.set-void.py
-echo Pin P009..........
-python.exe S2LP-srread.b.0x1001C-void.py
-echo ................07
-
-python.exe S2LP-srwrite.b.0x1001C.4-void.py
-echo ................04
-python.exe S2LP-srread.b.0x1001C-void.py
-echo ................04
-
-python.exe S2LP-pin.BEN_PG2.set-void.py
-echo Pin P505..........
-python.exe S2LP-srread.b.0x1001C-void.py
-echo ................04
-
-python.exe S2LP-srwrite.b.0x1001C.0-void.py
-echo ................00
-python.exe S2LP-srread.b.0x1001C-void.py
-echo ................00
-
-python.exe S2LP-srwrite.b.0x10100.1-void.py
-echo ................01
-python.exe S2LP-srread.b.0x10100-void.py
-echo ................01
-
-python.exe S2LP-stats-void.py
+echo THE END
