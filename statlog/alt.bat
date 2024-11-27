0f65a46be65b7b1d5090533a8cdca3f7b8cf4acd
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-11-26 21:17:08 -0500

    commented out all copy commands

--- statlog/alt.bat
+++ statlog/alt.bat
@@ -1,18 +1,18 @@
-xcopy statlog.py help.py /Y
-xcopy statlog.py zmax.py /Y
+rem xcopy statlog.py help.py /Y
+rem xcopy statlog.py zmax.py /Y
 
-xcopy statlog.py snread.py /Y
-xcopy statlog.py snread-fast.py /Y
-xcopy statlog.py S1LP-snread-fast.py /Y
+rem xcopy statlog.py snread.py /Y
+rem xcopy statlog.py snread-fast.py /Y
+rem xcopy statlog.py S1LP-snread-fast.py /Y
 
-xcopy statlog.py hist.py /Y
-xcopy statlog.py xlog.py /Y
-xcopy statlog.py cfg.py /Y
-xcopy statlog.py S2LP-cfg-fast.py /Y
+rem xcopy statlog.py hist.py /Y
+rem xcopy statlog.py xlog.py /Y
+rem xcopy statlog.py cfg.py /Y
+rem xcopy statlog.py S2LP-cfg-fast.py /Y
 
-xcopy statlog.py S2LP-fan.a.40-void.py /Y
-xcopy statlog.py S2LP-fan.a.60-void.py /Y
-xcopy statlog.py S2LP-fan.a.80-void.py /Y
-xcopy statlog.py S2LP-fan.a.100-void.py /Y
-xcopy statlog.py S2LP-fan-void.py /Y
-xcopy statlog.py S2LP-stats-void.py /Y
+rem xcopy statlog.py S2LP-fan.a.40-void.py /Y
+rem xcopy statlog.py S2LP-fan.a.60-void.py /Y
+rem xcopy statlog.py S2LP-fan.a.80-void.py /Y
+rem xcopy statlog.py S2LP-fan.a.100-void.py /Y
+rem xcopy statlog.py S2LP-fan-void.py /Y
+rem xcopy statlog.py S2LP-stats-void.py /Y
--- statlog/alt.sh
+++ statlog/alt.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-cp -f statlog.py help.py
-cp -f statlog.py zmax.py
+# cp -f statlog.py help.py
+# cp -f statlog.py zmax.py
 
-cp -f statlog.py snread.py
-cp -f statlog.py snread-fast.py
-cp -f statlog.py S1LP-snread-fast.py
+# cp -f statlog.py snread.py
+# cp -f statlog.py snread-fast.py
+# cp -f statlog.py S1LP-snread-fast.py
 
-cp -f statlog.py hist.py
-cp -f statlog.py xlog.py
-cp -f statlog.py cfg.py
-cp -f statlog.py S2LP-cfg-fast.py
+# cp -f statlog.py hist.py
+# cp -f statlog.py xlog.py
+# cp -f statlog.py cfg.py
+# cp -f statlog.py S2LP-cfg-fast.py
 
-cp -f statlog.py S2LP-fan.a.40-void.py
-cp -f statlog.py S2LP-fan.a.60-void.py
-cp -f statlog.py S2LP-fan.a.80-void.py
-cp -f statlog.py S2LP-fan.a.100-void.py
-cp -f statlog.py S2LP-fan-void.py
-cp -f statlog.py S2LP-stats-void.py
+# cp -f statlog.py S2LP-fan.a.40-void.py
+# cp -f statlog.py S2LP-fan.a.60-void.py
+# cp -f statlog.py S2LP-fan.a.80-void.py
+# cp -f statlog.py S2LP-fan.a.100-void.py
+# cp -f statlog.py S2LP-fan-void.py
+# cp -f statlog.py S2LP-stats-void.py
