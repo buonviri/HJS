f6e71d4ca098c589a2ce61ac0977fc99baeb98d9
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-12-02 15:07:27 -0500

    added comments, create file if it doesn't exist

--- u22/jam.sh
+++ u22/jam.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# store command sequence, timestamp, hex timestamp
 jamlog="$@"
 jamt=$(date +%s)
 jamhex=$(printf "%x" $jamt)
+
+# display info in terminal
 echo Writing to file...
+echo $jamhex
+echo $jamlog
+
+# check if file exists, create if not
+if [ ! -f ~/jam.info ]; then
+    echo "Timestamps and log entries:" > ~/jam.info
+fi
+
+# write info to file
 echo $jamhex $jamlog >> ~/jam.info
 
 # EOF
