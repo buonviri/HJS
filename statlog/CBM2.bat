c313d16151694756b95070e7a6a2d0b4cc5d1eea
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-12-04 16:58:30 -0500

    fixed pin number errors

--- statlog/CBM2.bat
+++ statlog/CBM2.bat
@@ -5,11 +5,11 @@ echo Compute Node A:
 python.exe statlog.py S2M2-srread.a.0x10100+srwrite.a.0x10100.0+srread.a.0x10100-void
 echo. 01 00 00
 python.exe statlog.py S2M2-pin.M2EN_PG1.set+srread.a.0x1001C-void
-echo. P203 07
+echo. P106 07
 python.exe statlog.py S2M2-srwrite.a.0x1001C.4+srread.a.0x1001C-void
 echo. 04 04 
 python.exe statlog.py S2M2-pin.M2EN_PG2.set+srread.a.0x1001C-void
-echo. P310 04
+echo. P111 04
 python.exe statlog.py S2M2-srwrite.a.0x1001C.0+srread.a.0x1001C+srwrite.a.0x10100.1+srread.a.0x10100-void
 echo. 00 00 01 01
 
