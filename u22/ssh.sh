7cbdfde714b84cce0ff03f9181c95a9a83d0467e
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2024-12-14 17:13:48 -0500

    missing slash n

--- u22/ssh.sh
+++ u22/ssh.sh
@@ -9,7 +9,7 @@ sudo ufw enable
 sudo ufw allow ssh
 echo
 
-printf "\e[1;35mTest using Windows: ssh ec@%s\e[0m" "$machine"
+printf "\e[1;35mTest using Windows: ssh ec@%s\e[0m\n" "$machine"
 
 echo
 
