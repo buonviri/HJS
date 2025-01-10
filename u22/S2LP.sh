d98713f3690305f41208442a779119141fb8349d
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-10 10:21:41 -0500

    added pauses

--- u22/S2LP.sh
+++ u22/S2LP.sh
@@ -3,11 +3,14 @@
 sudo echo PCIe status, compute blocks, etc...
 
 bmc
+sleep 1
 1fdc
+sleep 1
 cb12  # for S2LP
 cd ~/S2LP/install_mera && source start.sh && cd ~/S2LP/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
-mera --lssakura
 
+echo
+mera --lssakura
 echo
 
 # end
--- u22/S2M2.sh
+++ u22/S2M2.sh
@@ -3,11 +3,14 @@
 sudo echo PCIe status, compute blocks, etc...
 
 bmc
+sleep 1
 1fdc
+sleep 1
 cbm2  # for S2M2
 cd ~/S2M2/install_mera && source start.sh && cd ~/S2M2/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
-mera --lssakura
 
+echo
+mera --lssakura
 echo
 
 # end
