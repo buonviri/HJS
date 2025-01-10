4d2db4258428cb957ef69d8ac69df49b14610934
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-10 11:32:24 -0500

    s2xx versions

---
+++ u22/S2xx.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sudo echo PCIe status, compute blocks, etc...
+
+bmc
+1fdc
+cb12  # now works for both cards
+cd ~/S2xx/install_mera && source start.sh && cd ~/S2xx/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
+cd ~/S2xx
+
+echo
+mera --lssakura
+echo
+
+# end
