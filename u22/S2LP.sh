1b7b90b271aa60622a9d5ddd98b7f9c8a370273f
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-10 10:39:02 -0500

    changed to xx

--- u22/S2LP.sh
+++ u22/S2LP.sh
@@ -3,11 +3,9 @@
 sudo echo PCIe status, compute blocks, etc...
 
 bmc
-sleep 1
 1fdc
-sleep 1
 cb12  # for S2LP
-cd ~/S2LP/install_mera && source start.sh && cd ~/S2LP/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
+cd ~/S2xx/install_mera && source start.sh && cd ~/S2xx/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
 
 echo
 mera --lssakura
--- u22/S2M2.sh
+++ u22/S2M2.sh
@@ -3,11 +3,9 @@
 sudo echo PCIe status, compute blocks, etc...
 
 bmc
-sleep 1
 1fdc
-sleep 1
 cbm2  # for S2M2
-cd ~/S2M2/install_mera && source start.sh && cd ~/S2M2/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
+cd ~/S2xx/install_mera && source start.sh && cd ~/S2xx/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
 
 echo
 mera --lssakura
