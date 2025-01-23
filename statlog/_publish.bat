e9688285a93b5a813767f35e2a65f54800fec6f8
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-23 13:35:32 -0500

    removed /-I which doesn't work on WIN10

--- statlog/_publish.bat
+++ statlog/_publish.bat
@@ -1,12 +1,12 @@
 @echo off
 
 if exist C:\EdgeCortix\ (
-  xcopy bmc.bat C:\EdgeCortix\bmc.bat /Y /-I
-  xcopy cfg.bat C:\EdgeCortix\cfg.bat /Y /-I
-  xcopy info.bat C:\EdgeCortix\info.bat /Y /-I
-  xcopy stats.bat C:\EdgeCortix\stats.bat /Y /-I
-  xcopy stats.bat C:\EdgeCortix\s2.bat /Y /-I
-  xcopy xlog.bat C:\EdgeCortix\xlog.bat /Y /-I
+  xcopy bmc.bat C:\EdgeCortix\bmc.bat /Y
+  xcopy cfg.bat C:\EdgeCortix\cfg.bat /Y
+  xcopy info.bat C:\EdgeCortix\info.bat /Y
+  xcopy stats.bat C:\EdgeCortix\stats.bat /Y
+  xcopy stats.bat C:\EdgeCortix\s2.bat /Y
+  xcopy xlog.bat C:\EdgeCortix\xlog.bat /Y
 ) else (
   echo.
   echo.   'C:\EdgeCortix\' folder not found.
