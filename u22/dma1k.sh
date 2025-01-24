afda852d102fa199c8d62d1135d2a207aba51cd2
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-24 14:17:35 -0500

    filtered dma1k to match dma22

--- u22/dma1k.sh
+++ u22/dma1k.sh
@@ -11,25 +11,25 @@ echo
 echo Testing Device 0, DDR0...
 ~/S2LP/dna2_self_test_2_2_0/dma_test 0 ddr0 $size &> ~/.dma00
 # echo $?
-cat ~/.dma00 | \grep -i -E "speed" | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
+cat ~/.dma00 | \grep -i -E "passed|failed"  # | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
 
 echo
 echo Testing Device 0, DDR1...
 ~/S2LP/dna2_self_test_2_2_0/dma_test 0 ddr1 $size &> ~/.dma01
 # echo $?
-cat ~/.dma01 | \grep -i -E "speed" | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
+cat ~/.dma01 | \grep -i -E "passed|failed"  # | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
 
 echo
 echo Testing Device 1, DDR0...
 ~/S2LP/dna2_self_test_2_2_0/dma_test 1 ddr0 $size &> ~/.dma10
 # echo $?
-cat ~/.dma10 | \grep -i -E "speed" | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
+cat ~/.dma10 | \grep -i -E "passed|failed"  # | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
 
 echo
 echo Testing Device 1, DDR1...
 ~/S2LP/dna2_self_test_2_2_0/dma_test 1 ddr1 $size &> ~/.dma11
 # echo $?
-cat ~/.dma11 | \grep -i -E "speed" | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
+cat ~/.dma11 | \grep -i -E "passed|failed"  # | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
 
 rm ~/.dma*  # remove log files
 
