26223f05bf60b1f57d25ca49e0a7453f0b3f7c9f
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-23 21:44:22 -0500

    clarification

--- u22/antonio22.sh
+++ u22/antonio22.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo echo Running antonio.sh
+sudo echo Running antonio22.sh setup
 
 sudo apt install build-essential linux-headers-$(uname -r) gcc-12 libgoogle-glog0v5 -y
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt update && sudo apt-get install --only-upgrade libstdc++6 -y
