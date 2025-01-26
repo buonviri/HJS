23bd96f24d4770b3726554eaeb8906a880dec3ca
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-25 20:03:45 -0500

    Create prodtest.py

---
+++ mfg/prodtest.py
@@ -0,0 +1,21 @@
+from subprocess import Popen, PIPE
+
+process = Popen(['bmc'], stdout=PIPE, stderr=PIPE)  # sensors -j returns JSON format
+stdout, stderr = process.communicate()
+sensors = stdout.decode("utf-8")
+
+print(sensors)
+
+# notes:
+#   should we include error handling?
+#   is it better to remove all the unwanted entries prior to serial transmission?
+#   all we really need is a few temperatures and the fans, not all the limits and other info
+
+# -----------------------------------------------------
+
+# eval test for use on logging computer:
+import ast
+prodtest = ast.literal_eval(sensors)
+print(prodtest)
+
+# EOF
