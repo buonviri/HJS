76e6b3fc0b837a0b0b4c30989136a3e9324d4ab9
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-26 00:25:58 -0500

    green

--- mfg/prodtest.sh
+++ mfg/prodtest.sh
@@ -9,21 +9,21 @@ hexstamp=$(printf "%x" $time_t)
 sudo echo [ProdTest UTC $hexstamp] > ~/.prodtest-$hexstamp  # forces root login
 
 # serial in FTDI
-printf "Reading FTDI serial number (lsusb)\n"
+printf "\e[1;32m%b\e[0m" "Reading FTDI serial number (lsusb)\n"
 usbsn | awk '{$1=$1;print}' >> ~/.prodtest-$hexstamp  #  USB serial number
 
 # BMC: serial, version, PCIe
-printf "Reading BMC serial number / version / PCIe status (info and srread 0xC008C)\n"
+printf "\e[1;32m%b\e[0m"  "Reading BMC serial number / version / PCIe status (info and srread 0xC008C)\n"
 python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc  # write BMC serial and version to file
 python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc  # append PCIe info
 cat ~/.bmc | grep -i -E "variant|revision|c008c" | awk '{$1=$1;print}' >> ~/.prodtest-$hexstamp  #  variants and revisions
 
 # 1FDC
-printf "Reading OS info (lspci)\n"
+printf "\e[1;32m%b\e[0m" "Reading OS info (lspci)\n"
 1fdc | awk '{$1=$1;print}' >> ~/.prodtest-$hexstamp  # PCIe without leading spaces
 
 # ant22 and dma
-printf "Running all DMA tests...\n"
+printf "\e[1;32m%b\e[0m"  "Running all DMA tests...\n"
 ~/S2LP/dna2_self_test_2_2_0/setup_3pg.sh > /dev/null 2>&1  # hide all of the spam
 source ~/HJS/u22/dma00.sh >> ~/.prodtest-$hexstamp  # run all DMA tests
 
