a78a25d9006bc5b0406f6223340a2c262db228dd
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-25 21:39:57 -0500

    Update prodtest.sh

--- mfg/prodtest.sh
+++ mfg/prodtest.sh
@@ -8,8 +8,8 @@ echo timestamp: $hexstamp
 
 sudo echo [ProdTest] | tee ~/.prodtest-$hexstamp  # forces root login
 
-python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc | tee -a ~/.prodtest  # write info to file
-python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc | tee -a ~/.prodtest  # append PCIe info
+python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc | tee -a ~/.prodtest-$hexstamp  # write info to file
+python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc | tee -a ~/.prodtest-$hexstamp  # append PCIe info
 
 # DEBUG
 # echo
@@ -21,11 +21,11 @@ python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> 
 # echo
 # END
 
-cat ~/.bmc | grep -i -E "variant|revision|c008c" | awk '{$1=$1;print}' | tee -a ~/.prodtest  #  variants and revisions
-usbsn | awk '{$1=$1;print}' | tee -a ~/.prodtest  #  USB serial number
+cat ~/.bmc | grep -i -E "variant|revision|c008c" | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  variants and revisions
+usbsn | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  USB serial number
 
 # DEBUG
-1fdc | awk '{$1=$1;print}' | tee -a ~/.prodtest  # PCIe without leading spaces
+1fdc | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  # PCIe without leading spaces
 # END
 
 echo  # do not tee
