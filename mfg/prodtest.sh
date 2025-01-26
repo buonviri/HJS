140553314a70bbf59a20cf059f9d758679ebc672
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-25 21:59:16 -0500

    Update prodtest.sh

--- mfg/prodtest.sh
+++ mfg/prodtest.sh
@@ -8,27 +8,19 @@ hexstamp=$(printf "%x" $time_t)
 # echo timestamp: $hexstamp
 # END
 
+# start
 sudo echo [ProdTest $hexstamp] | tee ~/.prodtest-$hexstamp  # forces root login
 
-python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc | tee -a ~/.prodtest-$hexstamp  # write info to file
-python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc | tee -a ~/.prodtest-$hexstamp  # append PCIe info
-
-# DEBUG
-# echo
-# echo [ALL]
-# echo
-# cat ~/.bmc  # debug, dislplay file
-# echo
-# echo [/ALL]
-# echo
-# END
-
-cat ~/.bmc | grep -i -E "variant|revision|c008c" | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  variants and revisions
+# serial in FTDI
 usbsn | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  USB serial number
 
-# DEBUG
+# BMC: serial, version, PCIe
+python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc  # write BMC serial and version to file
+python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc  # append PCIe info
+cat ~/.bmc | grep -i -E "variant|revision|c008c" | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  variants and revisions
+
+# 1fdc
 1fdc | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  # PCIe without leading spaces
-# END
 
 echo  # do not tee
 # EOF
