29a06560e6d3546e3a61557e60ce597dc8ea60cd
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-25 21:17:57 -0500

    Update prodtest.sh

--- mfg/prodtest.sh
+++ mfg/prodtest.sh
@@ -2,15 +2,18 @@
 
 sudo echo [ProdTest]  # forces root login
 
+python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc  # write info to file
+python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc  # append PCIe info
 
-python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc
-python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc
-
+# DEBUG
 echo
 echo [ALL]
+echo
 cat ~/.bmc  # debug, dislplay file
+echo
 echo [/ALL]
 echo
+# END
 
 cat ~/.bmc | grep -i -E "variant|revision|c008c"  #  variants and revisions
 
