ae8e76c24de6003dc0632dceccf4dc7d237845e2
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-25 21:25:13 -0500

    Update prodtest.sh

--- mfg/prodtest.sh
+++ mfg/prodtest.sh
@@ -7,19 +7,19 @@ python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc  # write info to file
 python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc  # append PCIe info
 
 # DEBUG
-echo
-echo [ALL]
-echo
-cat ~/.bmc  # debug, dislplay file
-echo
-echo [/ALL]
-echo
+# echo
+# echo [ALL]
+# echo
+# cat ~/.bmc  # debug, dislplay file
+# echo
+# echo [/ALL]
+# echo
 # END
 
 cat ~/.bmc | grep -i -E "variant|revision|c008c"  #  variants and revisions
 
 # DEBUG
-1fdc | awk '{\$1=\$1;print}'  # PCIe without leading spaces
+1fdc | awk '{$1=$1;print}'  # PCIe without leading spaces
 # END
 
 echo
