d5fd746410c1a1a1e8b14302efdf522ae18034a4
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-25 22:02:58 -0500

    Update prodtest.sh

--- mfg/prodtest.sh
+++ mfg/prodtest.sh
@@ -12,14 +12,20 @@ hexstamp=$(printf "%x" $time_t)
 sudo echo [ProdTest $hexstamp] | tee ~/.prodtest-$hexstamp  # forces root login
 
 # serial in FTDI
+echo
+echo [get FTDI serial numnber]
 usbsn | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  USB serial number
 
 # BMC: serial, version, PCIe
+echo
+echo [get BMC serial, version PCIe]
 python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc  # write BMC serial and version to file
 python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc  # append PCIe info
 cat ~/.bmc | grep -i -E "variant|revision|c008c" | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  variants and revisions
 
 # 1fdc
+echo
+echo [get linux PCIe]
 1fdc | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  # PCIe without leading spaces
 
 echo  # do not tee
