e1b761a1f31628ea74ff54878b5e4e45ce70ebe8
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-25 22:08:13 -0500

    Update prodtest.sh

--- mfg/prodtest.sh
+++ mfg/prodtest.sh
@@ -18,7 +18,7 @@ usbsn | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  USB serial number
 
 # BMC: serial, version, PCIe
 echo
-echo [get BMC serial, version PCIe]
+echo [get BMC serial, version, PCIe]
 python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc  # write BMC serial and version to file
 python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc  # append PCIe info
 cat ~/.bmc | grep -i -E "variant|revision|c008c" | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  #  variants and revisions
@@ -29,4 +29,7 @@ echo [get linux PCIe]
 1fdc | awk '{$1=$1;print}' | tee -a ~/.prodtest-$hexstamp  # PCIe without leading spaces
 
 echo  # do not tee
+
+echo TEMP
+cat ~/.prodtest-$hexstamp
 # EOF
