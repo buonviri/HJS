80d35c600e30b8ae272e7693f9e09f499c308d50
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-25 23:50:13 -0500

    Update prodtest.sh

--- mfg/prodtest.sh
+++ mfg/prodtest.sh
@@ -13,7 +13,7 @@ printf "Reading FTDI serial number (lsusb)\n"
 usbsn | awk '{$1=$1;print}' >> ~/.prodtest-$hexstamp  #  USB serial number
 
 # BMC: serial, version, PCIe
-printf "Reading BMC serial, version, PCIe (info and srread 0xC008C)\n"
+printf "Reading BMC serial number / version / PCIe status (info and srread 0xC008C)\n"
 python3 ~/HJS/statlog/statlog.py S2XX-info-void > ~/.bmc  # write BMC serial and version to file
 python3 ~/HJS/statlog/statlog.py S2LP-srread.a.0xC008C+srread.b.0xC008C-void >> ~/.bmc  # append PCIe info
 cat ~/.bmc | grep -i -E "variant|revision|c008c" | awk '{$1=$1;print}' >> ~/.prodtest-$hexstamp  #  variants and revisions
