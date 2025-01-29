3a8f5fb3e8390b20ac426bc4f7ad74d1e77d2718
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-01-29 14:04:23 -0500

    Update sn.sh

--- mfg/sn.sh
+++ mfg/sn.sh
@@ -38,7 +38,7 @@ if [ $# == 2 ]; then
     printf "$total [$a0 $a1 $b0 $b1]\n"
 
     printf "Wr (GB/s):"
-    wr=$(cat $foobar$star | grep -i -o -P 'write speed =\K [0-9]\.[0-9]' | sed "s/\n/ /g")
+    wr=$(cat $foobar$star | grep -i -o -P 'write speed =\K [0-9]\.[0-9]' | sed "s/[[:space:]]//g")
     printf "$wr\n"
     #printf "Rd (GB/s):"
     #cat $foobar$star | grep -i -o -P 'read speed =\K [0-9]\.[0-9]' | sed "s/\n/ /g"
