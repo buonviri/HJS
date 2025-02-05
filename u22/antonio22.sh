d19d512ce3d8311f1cb8c29d9b4489e455975bbe
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-05 15:33:59 -0500

    attempted to remove S2LP from all paths

--- u22/antonio22.sh
+++ u22/antonio22.sh
@@ -7,8 +7,9 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt update && sudo a
 sudo ldconfig
 
 printf "\n\nAdditional instructions:\n\n"
-printf "Copy tar file to new folder ~/S2LP\n"
-printf "cd ~/S2LP && tar xvzf dna2_self_test_2_2_0.tar.gz && cd dna2_self_test_2_2_0\n"
+printf "Copy tar file to Home folder\n"
+printf "Copy and paste the following lines in Terminal:\n"
+printf "cd && tar xvzf dna2_self_test_2_2_0.tar.gz && cd dna2_self_test_2_2_0\n"
 printf "chmod +x ./setup_3pg.sh && ./setup_3pg.sh\n"
 printf "chmod +x ./run_3pg.sh && ./run_3pg.sh\n"
 
--- u22/dma22.sh
+++ u22/dma22.sh
@@ -9,25 +9,25 @@ printf "\e[1;31m   Hopefully you already typed 'ant22' !!!\e[0m\n"
 
 echo
 echo Testing Device 0, DDR0...
-~/S2LP/dna2_self_test_2_2_0/dma_test 0 ddr0 $size &> ~/.dma00
+~/dna2_self_test_2_2_0/dma_test 0 ddr0 $size &> ~/.dma00
 # echo $?
 cat ~/.dma00 | \grep -i -E "passed|failed"  # | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
 
 echo
 echo Testing Device 0, DDR1...
-~/S2LP/dna2_self_test_2_2_0/dma_test 0 ddr1 $size &> ~/.dma01
+~/dna2_self_test_2_2_0/dma_test 0 ddr1 $size &> ~/.dma01
 # echo $?
 cat ~/.dma01 | \grep -i -E "passed|failed"  # | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
 
 echo
 echo Testing Device 1, DDR0...
-~/S2LP/dna2_self_test_2_2_0/dma_test 1 ddr0 $size &> ~/.dma10
+~/dna2_self_test_2_2_0/dma_test 1 ddr0 $size &> ~/.dma10
 # echo $?
 cat ~/.dma10 | \grep -i -E "passed|failed"  # | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
 
 echo
 echo Testing Device 1, DDR1...
-~/S2LP/dna2_self_test_2_2_0/dma_test 1 ddr1 $size &> ~/.dma11
+~/dna2_self_test_2_2_0/dma_test 1 ddr1 $size &> ~/.dma11
 # echo $?
 cat ~/.dma11 | \grep -i -E "passed|failed"  # | sed "s/Read speed/RD/g" | sed "s/Write speed/WR/g"
 
