ed9de225eaae4b80c0f16eb291a503c31a726084
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-05 23:37:27 -0500

    s2clr now follows $mera rule

--- u22/mera22clr.sh
+++ u22/mera22clr.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# determine folder
+if [ -d ~/mera_package/ ]; then
+  mera="/home/ec/mera_package"  # use $HOME instead?
+else
+  mera="/home/ec/S2xx"  # this is only here to support legacy installations
+fi
+
 # show timestamp
 echo host time:
 date '+%H:%M:%S'
@@ -9,17 +16,17 @@ time_t=$(date +%s)
 hexstamp=$(printf "%x" $time_t)
 echo Filename suffix : $hexstamp
 
-mv ~/S2xx/examples/detr/result.png ~/S2xx/examples/detr/result-$hexstamp.png
-mv ~/S2xx/examples/efficient_net_demo/result.png ~/S2xx/examples/efficient_net_demo/result-$hexstamp.png
-mv ~/S2xx/examples/resnet50/result.png ~/S2xx/examples/resnet50/result-$hexstamp.png
+mv $mera/examples/detr/result.png $mera/examples/detr/result-$hexstamp.png
+mv $mera/examples/efficient_net_demo/result.png $mera/examples/efficient_net_demo/result-$hexstamp.png
+mv $mera/examples/resnet50/result.png $mera/examples/resnet50/result-$hexstamp.png
 
-mv ~/S2xx/examples/huggingface_image_classification/result.png ~/S2xx/examples/huggingface_image_classification/result-$hexstamp.png
-mv ~/S2xx/examples/huggingface_image_segmentation/result.png ~/S2xx/examples/huggingface_image_segmentation/result-$hexstamp.png
-mv ~/S2xx/examples/huggingface_text_classification/result.png ~/S2xx/examples/huggingface_text_classification/result-$hexstamp.png
-mv ~/S2xx/examples/huggingface_text_gen/result.png ~/S2xx/examples/huggingface_text_gen/result-$hexstamp.png
+mv $mera/examples/huggingface_image_classification/result.png $mera/examples/huggingface_image_classification/result-$hexstamp.png
+mv $mera/examples/huggingface_image_segmentation/result.png $mera/examples/huggingface_image_segmentation/result-$hexstamp.png
+mv $mera/examples/huggingface_text_classification/result.png $mera/examples/huggingface_text_classification/result-$hexstamp.png
+mv $mera/examples/huggingface_text_gen/result.png $mera/examples/huggingface_text_gen/result-$hexstamp.png
 
-mv ~/S2xx/examples/yolov5/result.png ~/S2xx/examples/yolov5/result-$hexstamp.png
-mv ~/S2xx/examples/yolov7/result.png ~/S2xx/examples/yolov7/result-$hexstamp.png
-mv ~/S2xx/examples/yolov8/result_1.png ~/S2xx/examples/yolov8/result_1-$hexstamp.png  # underscore one!
+mv $mera/examples/yolov5/result.png $mera/examples/yolov5/result-$hexstamp.png
+mv $mera/examples/yolov7/result.png $mera/examples/yolov7/result-$hexstamp.png
+mv $mera/examples/yolov8/result_1.png $mera/examples/yolov8/result_1-$hexstamp.png  # underscore one!
 
 # EOF
