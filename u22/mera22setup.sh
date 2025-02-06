604410a08ac89bd10ede53608187a7cfcb22334f
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-05 23:31:19 -0500

    comments only

--- u22/mera22setup.sh
+++ u22/mera22setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# this script assumes the mera tar file has been unzipped and placed in ~/S2xx already
-\cp -v ~/HJS/u22/results-mera22.html ~/S2xx/examples/results.html
+# this script assumes the mera tar file has been unzipped and placed in [home directory] already, per SOP
+\cp -v ~/HJS/u22/results-mera22.html ~/S2xx/examples/results.html  # HJS: legacy
 
 # need this once per system (removed picocom from original version):
 sudo apt install build-essential linux-headers-$(uname -r) gcc-12
@@ -9,7 +9,7 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test
 sudo apt update && sudo apt-get install --only-upgrade libstdc++6
 
 # install
-cd ~/S2xx/install_mera
+cd ~/S2xx/install_mera  # HJS: legacy
 ./install_os_dependencies.sh
 ./create_virtual_env.sh && source start.sh
 ./install_mera_and_python_dependencies.sh
@@ -17,7 +17,9 @@ cd ~/S2xx/install_mera
 ./install_mera_visualizer_and_python_dependencies.sh
 echo
 echo MERA 2.2 setup is complete
-echo Deactivating...
+echo Deactivating python virtual environment...
 deactivate
+
+printf "\nMove the S2xx folder to mera_package if you wish...\n"
 
 # EOF
