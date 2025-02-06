e49a3a338089c6077b29b8aa4dbecca69874f8a6
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-05 23:14:06 -0500

    s2i now uses mera var

--- u22/S2xx.sh
+++ u22/S2xx.sh
@@ -2,6 +2,13 @@
 
 sudo echo PCIe status, compute blocks, etc...
 
+# determine folder
+if [ -d ~/mera_package/ ]; then
+  mera="/home/ec/mera_package"  # use $HOME instead?
+else
+  mera="/home/ec/S2xx"  # this is only here to support legacy installations
+fi
+
 echo
 bmc
 echo
@@ -11,8 +18,8 @@ echo
 # cb12  # now works for both cards
 # echo
 
-cd ~/S2xx/install_mera && source start.sh && cd ~/S2xx/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
-cd ~/S2xx
+cd $mera/install_mera && source start.sh && cd $mera/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
+cd $mera
 
 echo
 mera --lssakura
