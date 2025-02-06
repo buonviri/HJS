9318f5f81935ead7cb01f68857b0536c43e7a6e8
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-05 22:50:44 -0500

    removed archived S2xx refs

--- u22/archive/S2LP.sh
+++ u22/archive/S2LP.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+# changed S2xx to [home directory]
+
 sudo echo PCIe status, compute blocks, etc...
 
 bmc
 1fdc
 cb12  # for S2LP
-cd ~/S2xx/install_mera && source start.sh && cd ~/S2xx/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
-cd ~/S2xx
+cd ~/[home directory]/install_mera && source start.sh && cd ~/S2xx/initialize_sakura_ii && chmod +x ./setup.sh && ./setup.sh
+cd ~/[home directory]
 
 echo
 mera --lssakura
