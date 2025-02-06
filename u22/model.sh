9ec978ac5c6eccf1d216400e995782373466806d
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-05 20:46:44 -0500

    clean up

--- u22/model.sh
+++ u22/model.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
+model=$1
+
+# determine folder
 if [ -d ~/mera_package/ ]; then
   mera="/home/ec/mera_package"
 else
   mera="/home/ec/S2xx"
 fi
-echo path is $mera
-echo arg is $1
-fullpath=$mera/examples/$1
-echo fullpath
-cd fullpath
+
+# calculate path and display info
+fullpath=$mera/examples/$model
+printf "Running %s in %s [%s]\n\n" "$1" "$mera" "$fullpath"
+
+# run
+cd $fullpath
 chmod +x ./run.sh
 ./run.sh
 
