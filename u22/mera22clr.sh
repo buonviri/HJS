8b5cd4fcd6b8c1fcec70f2b1777a39fe3cee7970
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-05 23:51:17 -0500

    added numbers

--- u22/mera22clr.sh
+++ u22/mera22clr.sh
@@ -15,18 +15,29 @@ date '+%H:%M:%S'
 time_t=$(date +%s)
 hexstamp=$(printf "%x" $time_t)
 echo Filename suffix : $hexstamp
+echo  # blank line
 
+printf "01) "
 mv -v $mera/examples/detr/result.png $mera/examples/detr/result-$hexstamp.png 2>/dev/null || echo OK
+printf "02 "
 mv -v $mera/examples/efficient_net_demo/result.png $mera/examples/efficient_net_demo/result-$hexstamp.png 2>/dev/null || echo OK
+printf "03) "
 mv -v $mera/examples/resnet50/result.png $mera/examples/resnet50/result-$hexstamp.png 2>/dev/null || echo OK
 
+printf "04) "
 mv -v $mera/examples/huggingface_image_classification/result.png $mera/examples/huggingface_image_classification/result-$hexstamp.png 2>/dev/null || echo OK
+printf "05) "
 mv -v $mera/examples/huggingface_image_segmentation/result.png $mera/examples/huggingface_image_segmentation/result-$hexstamp.png 2>/dev/null || echo OK
+printf "06) "
 mv -v $mera/examples/huggingface_text_classification/result.png $mera/examples/huggingface_text_classification/result-$hexstamp.png 2>/dev/null || echo OK
+printf "07) "
 mv -v $mera/examples/huggingface_text_gen/result.png $mera/examples/huggingface_text_gen/result-$hexstamp.png 2>/dev/null || echo OK
 
+printf "08) "
 mv -v $mera/examples/yolov5/result.png $mera/examples/yolov5/result-$hexstamp.png 2>/dev/null || echo OK
+printf "09) "
 mv -v $mera/examples/yolov7/result.png $mera/examples/yolov7/result-$hexstamp.png 2>/dev/null || echo OK
+printf "10) "
 mv -v $mera/examples/yolov8/result_1.png $mera/examples/yolov8/result_1-$hexstamp.png 2>/dev/null || echo OK  # filename contains underscore one!
 
 # EOF
