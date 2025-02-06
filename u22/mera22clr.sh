b21943937e18a703c52e2447bac06fdc70d81e4f
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-05 23:40:35 -0500

    more elegant clear

--- u22/mera22clr.sh
+++ u22/mera22clr.sh
@@ -16,17 +16,17 @@ time_t=$(date +%s)
 hexstamp=$(printf "%x" $time_t)
 echo Filename suffix : $hexstamp
 
-mv $mera/examples/detr/result.png $mera/examples/detr/result-$hexstamp.png
-mv $mera/examples/efficient_net_demo/result.png $mera/examples/efficient_net_demo/result-$hexstamp.png
-mv $mera/examples/resnet50/result.png $mera/examples/resnet50/result-$hexstamp.png
+mv $mera/examples/detr/result.png $mera/examples/detr/result-$hexstamp.png || echo OK
+mv $mera/examples/efficient_net_demo/result.png $mera/examples/efficient_net_demo/result-$hexstamp.png || echo OK
+mv $mera/examples/resnet50/result.png $mera/examples/resnet50/result-$hexstamp.png || echo OK
 
-mv $mera/examples/huggingface_image_classification/result.png $mera/examples/huggingface_image_classification/result-$hexstamp.png
-mv $mera/examples/huggingface_image_segmentation/result.png $mera/examples/huggingface_image_segmentation/result-$hexstamp.png
-mv $mera/examples/huggingface_text_classification/result.png $mera/examples/huggingface_text_classification/result-$hexstamp.png
-mv $mera/examples/huggingface_text_gen/result.png $mera/examples/huggingface_text_gen/result-$hexstamp.png
+mv $mera/examples/huggingface_image_classification/result.png $mera/examples/huggingface_image_classification/result-$hexstamp.png || echo OK
+mv $mera/examples/huggingface_image_segmentation/result.png $mera/examples/huggingface_image_segmentation/result-$hexstamp.png || echo OK
+mv $mera/examples/huggingface_text_classification/result.png $mera/examples/huggingface_text_classification/result-$hexstamp.png || echo OK
+mv $mera/examples/huggingface_text_gen/result.png $mera/examples/huggingface_text_gen/result-$hexstamp.png || echo OK
 
-mv $mera/examples/yolov5/result.png $mera/examples/yolov5/result-$hexstamp.png
-mv $mera/examples/yolov7/result.png $mera/examples/yolov7/result-$hexstamp.png
-mv $mera/examples/yolov8/result_1.png $mera/examples/yolov8/result_1-$hexstamp.png  # underscore one!
+mv $mera/examples/yolov5/result.png $mera/examples/yolov5/result-$hexstamp.png || echo OK
+mv $mera/examples/yolov7/result.png $mera/examples/yolov7/result-$hexstamp.png || echo OK
+mv $mera/examples/yolov8/result_1.png $mera/examples/yolov8/result_1-$hexstamp.png || echo OK  # filename contains underscore one!
 
 # EOF
