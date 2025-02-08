aca9977b05d68b2bc74d926982ae9595a44cf5c6
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-08 16:42:08 -0500

    Update lots.py

--- mfg/lots.py
+++ mfg/lots.py
@@ -4,7 +4,7 @@ import os
 import re
 
 location = '/home/ec/prodtest/'  # make this OS dependent eventually
-prodtest = '[0-9]{8}-.*\.txt'
+prodtest = '^[0-9]{8}-.*\.txt$'  # must start with eight digits and a dash, then any random chars, with .txt extension at the end
 
 print('Searching: ' + location)
 for dirname, dirnames, filenames in os.walk(location):
