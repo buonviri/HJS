9c317a982aa8cd24dd25d0975a9968c5f49cc62d
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-08 16:49:40 -0500

    Update lots.py

--- mfg/lots.py
+++ mfg/lots.py
@@ -5,11 +5,21 @@ import re
 
 location = '/home/ec/prodtest/'  # make this OS dependent eventually
 prodtest = '^[0-9]{8}-.*\.txt$'  # must start with eight digits and a dash, then any random chars, with .txt extension at the end
+info = {}  # blank dict for all lot code and serial number info
 
 print('Searching: ' + location)
 for dirname, dirnames, filenames in os.walk(location):
     for filename in filenames:
         # print(filename)
         if re.match(prodtest, filename):
-            print('lot = ' + filename[0:5] + ' sn = ' + filename[5:8])
+            lot = filename[0:5]
+            sn = filename[5:8]
+            print('lot = ' + lot + ' sn = ' + sn)
+            if lot not in info:
+                info[lot] = {}  # add empty dict
+            try:
+                info[lot][sn] = info[lot][sn] + 1  # attempt to increment counter
+            except:
+                info[lot][sn] = 1  # start with counter of one
+print(info)
 # EOF
