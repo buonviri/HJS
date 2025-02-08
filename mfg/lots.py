6f0d1c9309e52940846a670cb4bc5fc7f3580e78
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-08 16:39:42 -0500

    Update lots.py

--- mfg/lots.py
+++ mfg/lots.py
@@ -1,13 +1,15 @@
 # prints text, lot codess and SN ranges
 
 import os
+import re
 
 location = '/home/ec/prodtest/'  # make this OS dependent eventually
+prodtest = '[0-9]{8}-.*\.txt'
 
 print('Searching: ' + location)
 for dirname, dirnames, filenames in os.walk(location):
     for filename in filenames:
         # print(filename)
-        if filename.endswith('.txt'):
-            print('Good' + filename)
+        if re.match(prodtest, filename):
+            print(' ' + filename, end='')
 # EOF
