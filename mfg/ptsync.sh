638d2d20122c1034341c962db629833b86f2069e
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-10 11:15:01 -0500

    store return value

--- mfg/ptsync.sh
+++ mfg/ptsync.sh
@@ -10,40 +10,45 @@ printf "\nStoring prodtest files for: %s\n" "$host"
 
 printf "\nNavigate to prodtest:\n"
 cd ~/prodtest
-if [ $? -ne 0 ]; then
-  echo "   [error code $?]"
+foo=$?
+if [ $foo -ne 0 ]; then
+  echo "   [error code $foo]"
 else
   echo "   Done"
 fi
 
 printf "\nUpdate from github:\n"
 git pull
-if [ $? -ne 0 ]; then
-  echo "   [error code $?]"
+foo=$?
+if [ $foo -ne 0 ]; then
+  echo "   [error code $foo]"
 else
   echo "   Done"
 fi
 
 printf "\nAdd new files:\n" 
 git add .
-if [ $? -ne 0 ]; then
-  echo "   [error code $?]"
+foo=$?
+if [ $foo -ne 0 ]; then
+  echo "   [error code $foo]"
 else
   echo "   Done"
 fi
 
 printf "\nCommit:\n"
 git commit -m "$host"
-if [ $? -ne 0 ]; then
-  echo "   [error code $?]"
+foo=$?
+if [ $foo -ne 0 ]; then
+  echo "   [error code $foo]"
 else
   echo "   Done"
 fi
 
 printf "\nPush to github:\n"
 git push
-if [ $? -ne 0 ]; then
-  echo "   [error code $?]"
+foo=$?
+if [ $foo -ne 0 ]; then
+  echo "   [error code $foo]"
 else
   echo "   Done"
 fi
