1e069e1f556d9f795ee350f8b71cfa00d44a3d00
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-10 17:48:46 -0500

    moved add and injected error

--- mfg/ptsync.sh
+++ mfg/ptsync.sh
@@ -9,45 +9,45 @@ host=$(hostname)  # get hostname
 printf "\nStoring prodtest files for: %s\n" "$host"
 
 printf "\nNavigate to prodtest:\n"
-cd ~/prodtest
+cd ~/prodtest  # ------------------------------- cd
 foo=$?
 if [ $foo -ne 0 ]; then
   printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
 else
   echo "   Done"
   printf "\nUpdate from github:\n"
-  git pull
+  git pull  # ---------------------------------- pull
   foo=$?
   if [ $foo -ne 0 ]; then
     printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
   else
     echo "   Done"
     printf "\nAdd new files:\n" 
-    git add .
+    git addx .  # ------------------------------- add
     foo=$?
     if [ $foo -ne 0 ]; then
       printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
     else
       echo "   Done"
-    fi
-    printf "\nCommit:\n"
-    git commit -m "$host"
-    foo=$?
-    if [ $foo -ne 0 ]; then
-      printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
-    else
-      echo "   Done"
-    fi
-    printf "\nPush to github:\n"
-    git push
-    foo=$?
-    if [ $foo -ne 0 ]; then
-      printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
-    else
-      echo "   Done"
-    fi
-  fi
-fi
+      printf "\nCommit:\n"
+      git commit -m "$host"
+      foo=$?
+      if [ $foo -ne 0 ]; then
+        printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
+      else
+        echo "   Done"
+      fi
+      printf "\nPush to github:\n"
+      git push
+      foo=$?
+      if [ $foo -ne 0 ]; then
+        printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
+      else
+        echo "   Done"
+      fi  # end ?
+    fi  # end add
+  fi  # end pull
+fi  # end cd
 
 echo
 # EOF
