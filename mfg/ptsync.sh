467275b5fb9f00d03f920be4b6813420b6b06b5b
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-10 10:49:41 -0500

    added color to sync

--- mfg/ptsync.sh
+++ mfg/ptsync.sh
@@ -6,12 +6,27 @@
 # user has to pay attention to outcome!
 
 host=$(hostname)  # get hostname
-printf "\nStoring prodtest files for: %s\n\n" "$host"
+printf "\nStoring prodtest files for: %s\n" "$host"
 
+printf "\n\e[1;35m%b\e[0m\n" "Move to prodtest:"
 cd ~/prodtest
-git pull
-git add .
-git commit -m "$host"
-git push
+echo Status $?
 
+printf "\n\e[1;35m%b\e[0m\n" "Update from github:"
+git pull
+echo Status $?
+
+printf "\n\e[1;35m%b\e[0m\n" "Add new files:"
+git add .
+echo Status $?
+
+printf "\n\e[1;35m%b\e[0m\n" "Commit:"
+git commit -m "$host"
+echo Status $?
+
+printf "\n\e[1;35m%b\e[0m\n" "Push to github:"
+git push
+echo Status $?
+
+echo
 # EOF
