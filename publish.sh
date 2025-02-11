b20ce8adf5725270cb822d4fa7d564f11798f759
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-11 13:51:47 -0500

    Create publish.sh

---
+++ publish.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+function red () {
+  printf "\e[1;31m%b\e[0m" "$1"
+}
+
+function green () {
+  printf "\e[1;32m%b\e[0m" "$1"
+}
+
+# START OF SCRIPT
+echo
+
+if [ -d "/home/ec/hjs/" ]; then
+  red "Output folder already exists. Overwriting...\n"
+else
+  green "Creating output folder...\n"
+  mkdir -p "/home/ec/hjs/u22"
+  mkdir -p "/home/ec/hjs/mfg"
+  mkdir -p "/home/ec/hjs/statlog"
+fi
+cd "/home/ec/hjs/"
+
+foo="./.bash_aliases"  # new alias file
+printf "# %s [%s]\n" "$(date)" "$(hostname)" > $foo  # start with date/time and hostname
+cat "/home/ec/HJS/u22/edgecortix.bash_aliases" | grep -E 's2i=|bios=|usbsn=|ver=|pt=|xerr=|1fdc=|s2=' | sed 's/\/HJS\//\/hjs\//g' | tee -a $foo
+yes | \cp /home/ec/HJS/u22/S2xx.sh ./u22/
+yes | \cp /home/ec/HJS/u22/dma00d.sh ./u22/
+yes | \cp /home/ec/HJS/u22/dma00s.sh ./u22/
+yes | \cp /home/ec/HJS/statlog/statlog.py ./statlog/
+yes | \cp /home/ec/HJS/mfg/prodtest.sh ./mfg/
+sed -i 's/\/HJS\//\/hjs\//g' "./mfg/prodtest.sh"  # this script contains some paths
+cd "/home/ec/HJS/"  # return to folder where this script exists
+
+echo
+# EOF
