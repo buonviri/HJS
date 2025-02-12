187fe1d35f95da413ebb83806f9bc411519cd8e5
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-12 18:45:55 -0500

    clean up

--- mfg/trump.sh
+++ mfg/trump.sh
@@ -1,25 +1,23 @@
 #!/bin/bash
 
-s2i
+s2i  # initialize MERA
 cd ~/m.2_power_test
 printf "\e[1;35minit complete\e[0m -HJS\n"
 
 if [ $# -gt 0 ]; then  # one or more args passed
-  count=$1  # set count to arg
+  count=$1  # set count to first arg
 else
   count=1  # default to 1
 fi
 
 if [ $# == 2 ]; then 
-  query="--input_txt \"$2\""  # set query to arg
+  query="--input_txt \"$2\""  # set query to second arg
 else
   query="--input_txt \"Tell me about Trump.\""  # default
 fi
 
 for (( i=0; i<$count; i++ )); do
-  echo
-  printf "\e[1;32mi = $i\e[0m\n"
-  echo
+  printf "\n\e[1;32mi = $i\e[0m\n\n"
   echo "$query" | xargs python3 demo_model.py
 done
 
