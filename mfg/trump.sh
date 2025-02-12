cca0690da07938320013c4bf768b986b2ff666bf
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-12 18:13:02 -0500

    attempt at input text option

--- mfg/trump.sh
+++ mfg/trump.sh
@@ -4,17 +4,23 @@ s2i
 cd ~/m.2_power_test
 printf "\e[1;35minit complete\e[0m -HJS\n"
 
-if [ $# == 1 ]; then 
+if [ $# -gt 0 ]; then  # one or more args passed
   count=$1  # set count to arg
 else
   count=1  # default to 1
+fi
+
+if [ $# == 2 ]; then 
+  query="--input_txt '$2'"  # set query to arg
+else
+  query="--input_txt 'Tell me about Trump.'"  # default LOL
 fi
 
 for (( i=0; i<$count; i++ )); do
   echo
   printf "\e[1;32mi = $i\e[0m\n"
   echo
-  python3 demo_model.py
+  python3 demo_model.py "$query"
 done
 
 de
