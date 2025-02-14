881ad7b8e5b7ec02c1fc46c9af0ab7a06a17b03b
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-14 00:56:13 -0500

    removed one space

--- mfg/ptsync.sh
+++ mfg/ptsync.sh
@@ -11,37 +11,37 @@ printf "\nNavigate to prodtest:\n"
 cd ~/prodtest  # ------------------------------- cd
 foo=$?
 if [ $foo -ne 0 ]; then
-  printf "   \e[1;31m[EXIT: $foo]\e[0m\n"
+  printf "  \e[1;31m[EXIT: $foo]\e[0m\n"
 else
-  green "   [DONE]\n"
+  green "  [DONE]\n"
   printf "\nUpdate from github:\n"
   git pull  # ---------------------------------- pull
   foo=$?
   if [ $foo -ne 0 ]; then
-    printf "   \e[1;31m[EXIT: $foo]\e[0m\n"
+    printf "  \e[1;31m[EXIT: $foo]\e[0m\n"
   else
-    green "   [DONE]\n"
+    green "  [DONE]\n"
     printf "\nAdd new files:\n" 
     git add .  # ------------------------------- add
     foo=$?
     if [ $foo -ne 0 ]; then
-      printf "   \e[1;31m[EXIT: $foo]\e[0m\n"
+      printf "  \e[1;31m[EXIT: $foo]\e[0m\n"
     else
-      green "   [DONE]\n"
+      green "  [DONE]\n"
       printf "\nCommit:\n"
       git commit -m "$host"  # ----------------- commit
       foo=$?
       if [ $foo -ne 0 ]; then
-        printf "   \e[1;33m[EXIT: $foo]\e[0m\n"  # YELLOW
+        printf "  \e[1;33m[EXIT: $foo]\e[0m\n"  # YELLOW
       else
-        green "   [DONE]\n"
+        green "  [DONE]\n"
         printf "\nPush to github:\n"
         git push  # ---------------------------- push
         foo=$?
         if [ $foo -ne 0 ]; then
-          printf "   \e[1;31m[EXIT: $foo]\e[0m\n"
+          printf "  \e[1;31m[EXIT: $foo]\e[0m\n"
         else
-          green "   [DONE]\n"
+          green "  [DONE]\n"
         fi  # end push
       fi  # end commit
     fi  # end add
