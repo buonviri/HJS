cbb6fd8daa2f0e7a2df1ae412869d5841ae40a39
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-13 21:53:01 -0500

    removed parentheses

--- mfg/ptsync.sh
+++ mfg/ptsync.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# too many things can't handle this:
-# set -euo pipefail
-# IFS=$'\n\t'
-# user has to pay attention to outcome!
-
 function green () {
   printf "\e[1;32m%b\e[0m" "$1"
 }
@@ -16,35 +11,35 @@ printf "\nNavigate to prodtest:\n"
 cd ~/prodtest  # ------------------------------- cd
 foo=$?
 if [ $foo -ne 0 ]; then
-  printf "   \e[1;31m[EXIT ($foo)]\e[0m\n"
+  printf "   \e[1;31m[EXIT $foo]\e[0m\n"
 else
   green "   [DONE]\n"
   printf "\nUpdate from github:\n"
   git pull  # ---------------------------------- pull
   foo=$?
   if [ $foo -ne 0 ]; then
-    printf "   \e[1;31m[EXIT ($foo)]\e[0m\n"
+    printf "   \e[1;31m[EXIT $foo]\e[0m\n"
   else
     green "   [DONE]\n"
     printf "\nAdd new files:\n" 
     git add .  # ------------------------------- add
     foo=$?
     if [ $foo -ne 0 ]; then
-      printf "   \e[1;31m[EXIT ($foo)]\e[0m\n"
+      printf "   \e[1;31m[EXIT $foo]\e[0m\n"
     else
       green "   [DONE]\n"
       printf "\nCommit:\n"
       git commit -m "$host"  # ----------------- commit
       foo=$?
       if [ $foo -ne 0 ]; then
-        printf "   \e[1;31m[EXIT ($foo)]\e[0m\n"
+        printf "   \e[1;31m[EXIT $foo]\e[0m\n"
       else
         green "   [DONE]\n"
         printf "\nPush to github:\n"
         git push  # ---------------------------- push
         foo=$?
         if [ $foo -ne 0 ]; then
-          printf "   \e[1;31m[EXIT ($foo)]\e[0m\n"
+          printf "   \e[1;31m[EXIT $foo]\e[0m\n"
         else
           green "   [DONE]\n"
         fi  # end push
