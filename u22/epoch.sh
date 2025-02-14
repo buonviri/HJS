261dd9e716804db40a3f8e493276a2ae5fe703ea
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-13 23:33:17 -0500

    check arg count

--- u22/epoch.sh
+++ u22/epoch.sh
@@ -1,9 +1,13 @@
 #!/bin/basdh
 
-mydate=$1
-echo Converting $mydate...
-decimal=$(echo $((16#$mydate)))
-date -u -d +@$decimal
-date -d +@$decimal
+if [ "$#" -ne 1 ]; then
+  echo "Usage: epoch [hextimestamp]"
+else
+  mydate=$1
+  echo Converting $mydate...
+  decimal=$(echo $((16#$mydate)))
+  date -u -d +@$decimal
+  date -d +@$decimal
+fi
 
 # EOF
