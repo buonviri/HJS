bd0835796dda7be72f75ca63f88cda479d8c6c4a
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-13 22:49:43 -0500

    added fix

---
+++ u22/a22fix.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+sudo printf "\nRunning a22fix.sh\n\n"  # force root login
+
+cd ~/dna2_self_test_2_2_0/  # change to self test
+
+yes | \cp -v ~/HJS/mfg/setup_3pg.sh .
+chmod +x ./setup_3pg.sh
+yes | \cp -v ~/HJS/mfg/setup_3pg_none.sh .
+chmod +x ./setup_3pg_none.sh
+yes | \cp -v ~/HJS/mfg/setup_3pg_one.sh .
+chmod +x ./setup_3pg_one.sh
+yes | \cp -v ~/HJS/mfg/setup_3pg_zero.sh .
+chmod +x ./setup_3pg_zero.sh
+
+# EOF
