6b9312e61a3034813d097da4fce5739ca52f8a10
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-13 21:36:52 -0500

    green color in sync

--- mfg/ptsync.sh
+++ mfg/ptsync.sh
@@ -14,35 +14,35 @@ foo=$?
 if [ $foo -ne 0 ]; then
   printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
 else
-  echo "   [Done]"
+  echo "   \e[1;32m[Done]\e[0m"
   printf "\nUpdate from github:\n"
   git pull  # ---------------------------------- pull
   foo=$?
   if [ $foo -ne 0 ]; then
     printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
   else
-    echo "   [Done]"
+    echo "   \e[1;32m[Done]\e[0m"
     printf "\nAdd new files:\n" 
     git add .  # ------------------------------- add
     foo=$?
     if [ $foo -ne 0 ]; then
       printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
     else
-      echo "   [Done]"
+      echo "   \e[1;32m[Done]\e[0m"
       printf "\nCommit:\n"
       git commit -m "$host"  # ----------------- commit
       foo=$?
       if [ $foo -ne 0 ]; then
         printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
       else
-        echo "   [Done]"
+        echo "   \e[1;32m[Done]\e[0m"
         printf "\nPush to github:\n"
         git push  # ---------------------------- push
         foo=$?
         if [ $foo -ne 0 ]; then
           printf "   \e[1;31m[ERROR CODE $foo]\e[0m\n"
         else
-          echo "   [Done]"
+          echo "   \e[1;32m[Done]\e[0m"
         fi  # end push
       fi  # end commit
     fi  # end add
