ff11c10a48c5a5e1c7dc7d8e0042a44499aae9ab
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-17 20:53:37 -0500

    delete tar file note added

--- u22/antonio22.sh
+++ u22/antonio22.sh
@@ -8,7 +8,7 @@ sudo ldconfig
 
 printf "\n\nAdditional instructions:\n\n"
 printf "Copy tar file to Home folder and remove any letter suffix, e.g. 2_2_0b -> 2_2_0\n"
-printf "Copy and paste the following lines in Terminal and hit enter:\n"
+printf "Copy and paste the following lines in Terminal and hit enter. Delete tar file upon completion.\n"
 printf "cd && tar xvzf dna2_self_test_2_2_0.tar.gz && cd dna2_self_test_2_2_0\n"
 printf "chmod +x ./setup_3pg.sh && ./setup_3pg.sh\n"
 printf "chmod +x ./run_3pg.sh && ./run_3pg.sh\n"
