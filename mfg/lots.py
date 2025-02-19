49fc45a6f9a669909b07c798ce9104c60d1bd464
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-18 22:26:19 -0500

    pprint added

--- mfg/lots.py
+++ mfg/lots.py
@@ -2,6 +2,7 @@
 
 import os
 import re
+import pprint
 
 location = '/home/ec/prodtest/'  # make this OS dependent eventually
 prodtest = '^[0-9]{8}-.*\.txt$'  # must start with eight digits and a dash, then any random chars, with .txt extension at the end
@@ -49,7 +50,7 @@ for lot in lotlist:
     sns = []
     for sn in info[lot]:
         sns.append('  ' + sn + ' (x' + str(info[lot][sn]) + ')')  # store sn and count
-    print('\n'.join(sorted(sns)))
+    pprint.pprint('\n'.join(sorted(sns)))
 print()
 
 # EOF
