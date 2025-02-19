21583a08b22760488013f4b8cc4a42bf1b2fe5d9
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-19 15:30:46 -0500

    added 47 and 55 IMDT support

--- statlog/_publish.bat
+++ statlog/_publish.bat
@@ -7,6 +7,8 @@ if exist C:\EdgeCortix\ (
   xcopy stats.bat C:\EdgeCortix\stats.bat /Y
   xcopy stats.bat C:\EdgeCortix\s2.bat /Y
   xcopy xlog.bat C:\EdgeCortix\xlog.bat /Y
+  xcopy 47.bat C:\EdgeCortix\47.bat /Y
+  xcopy 55.bat C:\EdgeCortix\55.bat /Y
   xcopy 100.bat C:\EdgeCortix\100.bat /Y
   xcopy 110.bat C:\EdgeCortix\110.bat /Y
   xcopy 115.bat C:\EdgeCortix\115.bat /Y
