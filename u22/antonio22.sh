25da23d2e870b0b7303f9de668f1a7c48a33299e
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-19 20:56:18 -0500

    no longer remove 'b'

--- u22/antonio22.sh
+++ u22/antonio22.sh
@@ -7,9 +7,9 @@ sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt update && sudo a
 sudo ldconfig
 
 printf "\n\nAdditional instructions:\n\n"
-printf "Copy tar file to Home folder and remove any letter suffix, e.g. 2_2_0b -> 2_2_0\n"
+printf "Copy tar file to Home folder. Requires 'b' version!\n"
 printf "Copy and paste the following lines in Terminal and hit enter. Delete tar file upon completion.\n"
-printf "cd && tar xvzf dna2_self_test_2_2_0.tar.gz && cd dna2_self_test_2_2_0\n"
+printf "cd && tar xvzf dna2_self_test_2_2_0b.tar.gz && cd dna2_self_test_2_2_0\n"
 printf "chmod +x ./setup_3pg.sh && ./setup_3pg.sh\n"
 printf "chmod +x ./run_3pg.sh && ./run_3pg.sh\n"
 
