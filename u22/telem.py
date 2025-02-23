a032f4df692d69e1517ff0ae5e4681d7dba62335
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-22 23:53:33 -0500

    try rb

--- u22/telem.py
+++ u22/telem.py
@@ -1,6 +1,6 @@
 # formatted telemetry
 
-with open('telem.info', 'r', encoding='utf-8') as f:  # utf-8 allows non-ascii chars
+with open('telem.info', 'rb') as f:
     telem = f.read()
 print(telem.printable())
 
