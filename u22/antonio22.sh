3f89ad2bd0be0ce41d0b26a398016326f479df87
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-22 19:31:20 -0500

    added fix script

--- u22/antonio22.sh
+++ u22/antonio22.sh
@@ -6,4 +6,6 @@ sudo apt install build-essential linux-headers-$(uname -r) gcc-12 libgoogle-glog
 sudo add-apt-repository ppa:ubuntu-toolchain-r/test && sudo apt update && sudo apt-get install --only-upgrade libstdc++6 -y
 sudo ldconfig
 
+source ~/HJS/u22/a22fix.sh  # fix bugs
+
 # EOF
