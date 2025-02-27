8edf6505339b7ac66bc0fc9bf3b5fafe9c84a5a3
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-02-27 13:22:06 -0500

    warning and ending

--- u22/xload.sh
+++ u22/xload.sh
@@ -4,6 +4,7 @@ function purple () {
   printf "\e[1;35m%b\e[0m" "$1"
 }
 
+# initial warning
 echo
 purple "This process may take up to a minute. Do not access the serial port until it completes.\n"
 echo
@@ -20,6 +21,9 @@ echo "[Sending hex file]"
 echo
 cat ./S112.hex | picocom -qrix 1000 /dev/ttyUSB0
 
+# requires poweroff
+echo
+echo Cycle power to boot the new image.
 echo
 
 # End
