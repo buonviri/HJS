2178486d361353efe524d697e8c888f4a16dfee6
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-03-01 14:45:49 -0500

    added cfg to publish. untested.

--- publish.sh
+++ publish.sh
@@ -24,7 +24,7 @@ cd "/home/ec/hjs/"
 printf "\nAliases:\n"
 foo="./.bash_aliases"  # new alias file
 printf "# %s [%s]\n" "$(date)" "$(hostname)" > $foo  # start with date/time and hostname
-cat "/home/ec/HJS/u22/edgecortix.bash_aliases" | grep -E ' s2i=| bios=| usbsn=| ver=| prodtest=| pt=| xlogver=| xlogslow=| xerr=| 1fdc=| s2=| info=| c008c=| dmadual=| dmasingle=| dryi=| dry=| enpg=' | sed 's/\/HJS\//\/hjs\//g' | tee -a $foo
+cat "/home/ec/HJS/u22/edgecortix.bash_aliases" | grep -E ' s2i=| bios=| usbsn=| ver=| prodtest=| pt=| xlogver=| xlogslow=| xerr=| 1fdc=| s2=| info=| c008c=| dmadual=| dmasingle=| dryi=| dry=| enpg=| cfga=| cfgb=| cfg4pt=' | sed 's/\/HJS\//\/hjs\//g' | tee -a $foo
 
 printf "\nScripts:\n"
 yes | \cp -v /home/ec/HJS/u22/S2xx.sh ./u22/
@@ -32,9 +32,7 @@ yes | \cp -v /home/ec/HJS/u22/dma00d.sh ./u22/
 yes | \cp -v /home/ec/HJS/u22/dma00s.sh ./u22/
 yes | \cp -v /home/ec/HJS/statlog/statlog.py ./statlog/
 yes | \cp -v /home/ec/HJS/mfg/prodtest.sh ./mfg/
-
-# no longer needed
-# sed -i 's/\/HJS\//\/hjs\//g w /dev/stdout' "./mfg/prodtest.sh"  # this script contains some paths
+yes | \cp -v /home/ec/HJS/mfg/cfg4pt.py ./mfg/
 
 cd "/home/ec/HJS/"  # return to folder where this script exists
 echo
