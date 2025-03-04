840c874f887b7cf8d433f4201f25dc4d397daea5
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-03-03 23:48:01 -0500

    hard code delay

--- mfg/bistn.sh
+++ mfg/bistn.sh
@@ -3,7 +3,7 @@
 if [ $# == 1 ]; then 
   count=$1
   for (( i=0; i<$count; i++ )); do
-    source ~/HJS/mfg/bist.sh
+    source ~/HJS/mfg/bist.sh 4000  # hopefully this is a good delay
   done
 else
   printf "Usage:\n   Enter the number of loops.\nExample:\n   bistn 10\n"
