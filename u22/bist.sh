fbf3baa84834135bd0dd6a7b6fdd1de97940a1ef
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-03-03 21:39:40 -0500

    draft of bist

---
+++ u22/bist.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# == 1 ]; then  # one arg was passed
+  hexver="$1"  # set to arg
+else
+  hexver="S112"  # default
+fi
+
+function purple () {
+  printf "\e[1;35m%b\e[0m" "$1"
+}
+
+# initial warning
+echo
+purple "This process may take a while. Do not access the serial port until it completes.\n"
+echo
+
+# set up picocom
+picocom -qrX -b 115200 --flow x --send-cmd ascii-xfr /dev/ttyUSB0
+
+# send bist command
+echo "bist" | picocom -qrix 1000 /dev/ttyUSB0
+
+# requires poweroff
+echo Done.
+echo
+
+# End
