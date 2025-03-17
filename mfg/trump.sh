bf66615efac623821417e6bc01848c290cdc6f6f
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-03-16 22:53:49 -0400

    one dp

--- mfg/trump.sh
+++ mfg/trump.sh
@@ -24,7 +24,7 @@ for (( i=0; i<$count; i++ )); do
   now=$(date +%s%4N)
   elapsedms=$((now-start))  # calc elapsed
   elapsed=$(echo "$elapsedms/10000.0" | bc -l)
-  printf "\n\e[1;32mi = $i\nt = %.3f s\e[0m\n\n" "$elapsed"
+  printf "\n\e[1;32mi = $i\nt = %.1f s\e[0m\n\n" "$elapsed"
   echo "$query" | xargs python3 demo_model.py
 done
 
@@ -36,9 +36,9 @@ now=$(date +%s%4N)
 elapsedms=$((now-start))  # calc elapsed
 elapsed=$(echo "$elapsedms/10000.0" | bc -l)
 if [ $count == 1 ]; then
-  printf "\n\e[1;32mCompleted 1 query.\nt = %.3f s\e[0m\n\n" "$elapsed"
+  printf "\n\e[1;32mCompleted 1 query.\nt = %.1f s\e[0m\n\n" "$elapsed"
 else
-  printf "\n\e[1;32mCompleted $i queries.\nt = %.3f s\e[0m\n\n" "$elapsed"
+  printf "\n\e[1;32mCompleted $i queries.\nt = %.1f s\e[0m\n\n" "$elapsed"
 fi
 
 # EOF
