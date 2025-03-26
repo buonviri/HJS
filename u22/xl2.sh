267c0bad40a7b3241147684598a2d8559bc12e27
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-03-25 22:58:10 -0400

    xl2 help finished

--- u22/xl2.sh
+++ u22/xl2.sh
@@ -30,50 +30,52 @@ function purple () {
   printf "\e[1;35m%b\e[0m" "$1"
 }
 
-# initial warning
+# only finish script if help was not passed
 if [ "$help" == "0" ]; then 
+  # initial warning
   echo
   purple "This process may take up to a minute. Do not access the serial port until it completes.\n"
   echo
+
   # change baud rate
   python3 ~/HJS/statlog/statlog.py S2XX-baud.$baudx | \grep "baud "  # >> /dev/null
-fi
 
-# set up picocom
-if [ -e /dev/ttyUSB0 ]; then
-  picocom -qrX -b $baud --flow x --send-cmd ascii-xfr /dev/ttyUSB0
-else
-  red "/dev/ttyUSB0 is not connected"
-fi
+  # set up picocom
+  if [ -e /dev/ttyUSB0 ]; then
+    picocom -qrX -b $baud --flow x --send-cmd ascii-xfr /dev/ttyUSB0
+  else
+    red "/dev/ttyUSB0 is not connected"
+  fi
 
-# send xload 1 command
-if [ -e /dev/ttyUSB0 ]; then
-  echo "xload 1" | picocom -qrix 1000 /dev/ttyUSB0
-fi
+  # send xload 1 command
+  if [ -e /dev/ttyUSB0 ]; then
+    echo "xload 1" | picocom -qrix 1000 /dev/ttyUSB0
+  fi
 
-# send hex file
-start=$(date +%s)
-if [ -e /dev/ttyUSB0 ]; then
+  # send hex file
+  start=$(date +%s)
+  if [ -e /dev/ttyUSB0 ]; then
+    echo
+    echo "[Sending file $folder/$hexver.hex]"
+    echo
+    cat /home/ec/hex-ftdi-cfg/$folder/$hexver.hex | picocom -qrix 1000 /dev/ttyUSB0
+  fi
+  end=$(date +%s)
+  elapsed=$((end-start))
+
+  # change baud rate, requires br3x suffix
   echo
-  echo "[Sending file $folder/$hexver.hex]"
+  python3 ~/HJS/statlog/statlog.py S2XX-baud.1-$undo | \grep "baud "  # >> /dev/null
+  echo "Transfer time = $elapsed s"
+
+  # requires poweroff
+  if [ -e /dev/ttyUSB0 ]; then
+    echo Cycle power to boot the new image.
+  fi
   echo
-  cat /home/ec/hex-ftdi-cfg/$folder/$hexver.hex | picocom -qrix 1000 /dev/ttyUSB0
+
+  # play sound
+  a440
 fi
-end=$(date +%s)
-elapsed=$((end-start))
-
-# change baud rate, requires br3x suffix
-echo
-python3 ~/HJS/statlog/statlog.py S2XX-baud.1-$undo | \grep "baud "  # >> /dev/null
-echo "Transfer time = $elapsed s"
-
-# requires poweroff
-if [ -e /dev/ttyUSB0 ]; then
-  echo Cycle power to boot the new image.
-fi
-echo
-
-# play sound
-a440
 
 # End
