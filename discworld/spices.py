752fe5a82d906a43cee508639a27b355d391ef42
buonviri <andy@buonviri.com>
2025-04-06 02:10:09 -0400

    removed old info

--- discworld/spices.py
+++ discworld/spices.py
@@ -1,3 +1,4 @@
+import os
 import pyperclip
 
 filtered = []
@@ -31,35 +32,6 @@ for line in handfuls:
         print(line)
 pyperclip.copy('\n'.join(filtered) + '\n' + '\n'.join(discard))
 
-"""
-You count about three handfuls of ground garlic inside the large wardrobe, about nineteen handfuls of ground basil inside the large wardrobe, about eight handfuls of cloves inside the large wardrobe, about one handful of ground parsley inside the large wardrobe, about ten handfuls of ground thyme inside the large wardrobe, about seven handfuls of some curry powder inside the large wardrobe, about thirteen handfuls of ground oregano inside the large wardrobe, about nine handfuls of black pepper inside the large wardrobe, about eleven handfuls of ground sage inside the large wardrobe, about five handfuls of ground cinnamon inside the large wardrobe, about two handfuls of ground turmeric inside the large wardrobe, about twenty-one handfuls of ground coriander inside the large wardrobe, about five handfuls of ground cardamom inside the large wardrobe, about eight handfuls of sumac inside the large wardrobe, about fourteen handfuls of methi inside the large wardrobe, about sixteen handfuls of ground ginger inside the large wardrobe, about eight handfuls of ground fennel inside the large wardrobe, about six handfuls of caraway inside the large wardrobe, about seven handfuls of cumin inside the large wardrobe and about ten handfuls of saffron inside the large wardrobe with a total of one hundred and eighty-three items.
-You count about seven handfuls of some curry powder inside the large wardrobe with a total of seven items.
-You count about seven handfuls of sea salt inside the large wardrobe with a total of seven items.
+os.system("PAUSE")
 
-about three handfuls of ground garlic inside the large wardrobe
-about nineteen handfuls of ground basil inside the large wardrobe
-about eight handfuls of cloves inside the large wardrobe
-about one ***HANDFUL*** of ground parsley inside the large wardrobe
-about ten handfuls of ground thyme inside the large wardrobe
-about seven handfuls of some curry powder inside the large wardrobe
-about thirteen handfuls of ground oregano inside the large wardrobe
-about nine handfuls of black pepper inside the large wardrobe
-about eleven handfuls of ground sage inside the large wardrobe
-about five handfuls of ground cinnamon inside the large wardrobe
-about two handfuls of ground turmeric inside the large wardrobe
-about twenty-one handfuls of ground coriander inside the large wardrobe
-about five handfuls of ground cardamom inside the large wardrobe
-about eight handfuls of sumac inside the large wardrobe
-about fourteen handfuls of methi inside the large wardrobe
-about sixteen handfuls of ground ginger inside the large wardrobe
-about eight handfuls of ground fennel inside the large wardrobe
-about six handfuls of caraway inside the large wardrobe
-about seven handfuls of cumin inside the large wardrobe
-about ten handfuls of saffron inside the large wardrobe
-about seven handfuls of some curry powder inside the large wardrobe
-about seven handfuls of sea salt inside the large wardrobe
-one hundred and eighty-three items
-seven items
-seven items
-
-"""
+#EOF
