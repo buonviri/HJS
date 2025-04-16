99501768a62e522c41c66d982b71a1011c40bdc7
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-04-15 20:10:50 -0400

    added regression, and 8 vs 12 option

--- barcode/read8.py
+++ barcode/read8.py
@@ -1,12 +1,21 @@
+import sys
 import msvcrt
 
 separator = '-EC-'
 
 def getbarcode():
     bc = ""
-    while len(bc) < 8:
+    n = 8
+    while len(bc) < n:
         c = msvcrt.getwch() # reads one unicode char at a time
         bc += c
+        if len(bc) == 6:
+            if bc[5].isdigit():
+                n = 8
+            elif bc[5] in '-_':  # separators for n=12, typically dash or underscore
+                n = 12
+            else:
+                pass  # could add more checks
     return bc
 # End
 
@@ -15,17 +24,32 @@ def validate(bc):
         return 'Cannot have leading zeroes'
     elif bc.startswith('0'):
         return 'Cannot have a leading zero'
-    elif not bc.isdigit():
-        return 'Must contain only decimal digits [0-9]'
+    elif len(bc) == 8:
+        if not bc.isdigit():
+            return 'Must contain only decimal digits [0-9]'
+    elif len(bc) == 12:
+        if not bc[0:5].isdigit():
+            return 'Lot code must contain only decimal digits [0-9]'
+        elif not bc[9:12].isdigit():
+            return 'Serial must contain only decimal digits [0-9]'
+        elif bc[5:9] != separator:
+            return 'Separator must be "' + separator + '"'
     return ''
 # End
 
 # Start of script
-barcode = getbarcode()
+print()  # blank line
+if len(sys.argv) == 2:  # exactly one arg
+    barcode = sys.argv[1]  # barcode is only arg
+    print('[' + barcode + '] ', end ='')
+else:
+    barcode = getbarcode()  # read from input
 bcerror = validate(barcode)
 if bcerror == '':  # no error
-    print('Writing files for: ' + barcode[0:5] + separator + barcode[5:8])
+    if len(barcode) == 8:
+        print('Writing files for: ' + barcode[0:5] + separator + barcode[5:8])
+    elif len(barcode) == 12:
+        print('Writing files for: ' + barcode)
 else:
-    print('Invalid input (' + barcode + ') - ' + bcerror)
-
-# Add pause?
+    print('Invalid input (' + barcode + ') | ' + bcerror)
+# print()  # blank line
