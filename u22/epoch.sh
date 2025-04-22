485e953851c182083bbf0563c4d0072c280ce015
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-04-21 21:36:56 -0400

    comments

--- u22/epoch.sh
+++ u22/epoch.sh
@@ -6,8 +6,8 @@ else
   mydate=$1
   echo Converting $mydate...
   decimal=$(echo $((16#$mydate)))
-  date -u -d +@$decimal
-  date -d +@$decimal
+  date -u -d +@$decimal  # UTC
+  date -d +@$decimal  # local time
 fi
 
 # EOF
