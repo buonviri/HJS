7573d8c458c25176f68fbd3eb836e451687b16b6
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-10-01 00:10:02 -0400

    changed indents

--- grep.py
+++ grep.py
@@ -3,29 +3,29 @@
 import os
 import sys
 
-prefix = '\ngrep '  # first printed line
+prefix = '\ngrep'  # first printed line, prefix
 if len(sys.argv) == 3:
     filename = sys.argv[1]
     pattern = sys.argv[2]
-    result = '[in] ' + filename + ' [find] ' + pattern
+    result = ' [in] ' + filename + ' [find] ' + pattern
     search = True
 else:
-    result = 'syntax is grep.py filename pattern'
+    result = ' syntax is grep.py filename pattern'
     search = False
 print(prefix + result)
 
-if search:
+if search:  # only do this if filename and pattern exist
     matches = 0
     try:
         with open(filename, 'r') as f:
             lines = f.readlines()
         for line in lines:
             if pattern in line:
-                print(' |' + line.rstrip() + '|')  # remove trailing spaces only
+                print('  |' + line.rstrip() + '|')  # remove trailing spaces only, add indent
                 matches = matches + 1
-        print('Matches: ' + str(matches))
+        print('  Matches: ' + str(matches))  # indent
     except:
-        print('File not found')
+        print('  File not found')  # indent
 
 # os.system("PAUSE")  # handled in batch file
 
