fe01d481aaba0ecbf79104e46ddfe04d920baa34
andybuonviri <160339851+andybuonviri@users.noreply.github.com>
2025-10-16 20:48:31 -0400

    added DRY

--- mfg/ae2ss.py
+++ mfg/ae2ss.py
@@ -1,12 +1,14 @@
 # ae2ss generates a dictionary and spreadsheet(s) based on all of the aetina log files in the current folder
 
 import os
+import pyperclip
 
-ver = '0.40'  # add min/max
+ver = '0.50'  # DRY, tabs, remove file extension
 
 info = {}  # good info
 sn_min = 99999999
 sn_max = 0
+clip = ''  # clipboard
 
 def summarize(lines, dirname, filename):
     global sn_min
@@ -14,6 +16,8 @@ def summarize(lines, dirname, filename):
     s = ''  # blank file
     bmc = ''
     serial_lines = ''
+    dry_lines = ''
+    dry_lines_dupe = ''
     bmc_info = {  # keys are prodtest string, values are summary entries
         'Secondary image, Revision 1.1.5': '[S115]',
         'Secondary image, Revision 2.0.3': '[S203]',
@@ -29,27 +33,40 @@ def summarize(lines, dirname, filename):
         sn_min = sn_int
     if sn_int > sn_max:
         sn_max = sn_int
-    s = s + filename + '\n'  # add raw filename
-    s = s + sn_file + ' '  # add SN (should be first eight or twelve chars of filename) and trailing space
+    s = s + filename[:-4] + '\t'  # add raw filename minus extension
+    s = s + sn_file  # add SN (should be first eight or twelve chars of filename)
     for line in lines:
         line = line.strip()
         if line.startswith('FTDI: ') and sn_file in line:  # FTDI, correct XXXXXYYY was found
             serial_lines = serial_lines + 'F'
-        if line.startswith('iSerial 3 ') and sn_file in line:  # iSerial 3, correct XXXXXYYY was found
+        elif line.startswith('iSerial 3 ') and sn_file in line:  # iSerial 3, correct XXXXXYYY was found
             serial_lines = serial_lines + 'i'
-        if line.startswith('Board: ') and sn_ec in line:  # BMC info, correct XXXXX-EC-YYY was found
+        elif line.startswith('Board: ') and sn_ec in line:  # BMC info, correct XXXXX-EC-YYY was found
             serial_lines = serial_lines + 'B'
+        elif '[DETR]' in line:  # DRY section
+            if len(dry_lines) > 0:  # match was already found
+                dry_lines_dupe = dry_lines_dupe + dry_lines
+                dry_lines = ''
+            dry_lines = dry_lines + '\tD'
+        elif '[ResNet50]' in line:  # DRY section
+            dry_lines = dry_lines + '\tR'
+        elif '[YoloV8]' in line:  # DRY section
+            dry_lines = dry_lines + '\tY'
+        elif 'Total latency: ' in line and line.endswith(' us'):  # DRY latency
+            dry_latency = line.split()[-2]  # store second to last string
+            dry_lines = dry_lines + '\t' + '%.0f' % float(dry_latency)
         for bmc_key in bmc_info:
             if bmc_key in line:
                 bmc = bmc_info[bmc_key]
-    s = s + serial_lines  # add all SN chars
-    s = s + bmc  # add BMC string
+    s = s + dry_lines  # add all DRY chars
+    s = s + '\t' + bmc  # add BMC string
+    s = s + '\t' + serial_lines  # add all SN chars
+    s = s + dry_lines_dupe  # add duplicate(s)
     print(s)  # debug
-    with open('summary.txt', 'a') as f:
-        f.write(s + '\n')
+    return s
 
 # clear summary in case it already exists
-with open('summary.txt', 'w') as f:
+with open('_summary.txt', 'w') as f:
     f.write(os.path.abspath(os.getcwd()) + '\n')  # path
     f.write('ae2ss version ' + ver + '\n\n')  # version number
 
@@ -60,12 +77,18 @@ for dirname, dirnames, filenames in os.walk('.'):  # get info from current folde
         if filename.endswith('.log'):  # check extension
             filecount = filecount + 1
             with open(os.path.join(dirname,filename), 'r', encoding='utf-8') as f:
-                summarize(f.readlines(), dirname, filename)
+                result = summarize(f.readlines(), dirname, filename)
+                with open('_summary.txt', 'a') as f:
+                    f.write(result + '\n')
+                    clip = clip + result + '\n'
 
 # summarize counts
-with open('summary.txt', 'a') as f:
+with open('_summary.txt', 'a') as f:
     f.write('\nSN ' + str(sn_min) + ' to ' + str(sn_max) + ' (' + str(sn_max-sn_min+1) + ')\n')  # sn range and expected count
 print('\nFile count: ' + str(filecount))  # print file count
+
+# cilpboard
+pyperclip.copy(clip)  # place result on clipboard
 
 # EOF
 
