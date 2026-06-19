b25ef06fe3786015ea871c481c0519988ff52b2c
buonviri <andy@buonviri.com>
2026-06-19 02:21:32 -0400

    tables allowed, 1/1/2 item count, remove period

--- discworld/shop.py
+++ discworld/shop.py
@@ -6,13 +6,14 @@ import pyperclip
 filtered = []
 discard = []
 clip = pyperclip.paste()
-required = {
-    'grey pleated skirt': True,
-}
 
 clip = clip.replace('You count', '')
+clip = clip.replace('On the elderly kitchen table are', '')
 clip = clip.replace('with a total of', ',')
 clip = clip.replace('\n', ',')
+clip = clip.replace('and a', ', a')  # serial comma workaround
+clip = clip.replace('and one', ', one')  # serial comma workaround
+clip = clip.replace('and two', ', two')  # serial comma workaround
 clip = clip.replace('and three', ', three')  # serial comma workaround
 clip = clip.replace('and four', ', four')  # serial comma workaround
 clip = clip.replace('and five', ', five')  # serial comma workaround
@@ -31,6 +32,7 @@ for line in items:
     elif line.endswith(' items.'):
         pass  # meaningless line
     else:
+        line=line.replace('.','')  # remove ending period, might remove others as well
         filtered.append(line.strip())
         tee(line)
 
