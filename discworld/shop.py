941720f351386b5bdf0d67c9bbbf6cf0446475ab
buonviri <andy@buonviri.com>
2026-06-19 01:36:59 -0400

    start of shop.py parser for barrel and tables

---
+++ discworld/shop.py
@@ -0,0 +1,40 @@
+# copy results of 'shop' alias to clipboard, then run this script
+
+import os
+import pyperclip
+
+filtered = []
+discard = []
+clip = pyperclip.paste()
+required = {
+    'grey pleated skirt': True,
+}
+
+clip = clip.replace('You count', '')
+clip = clip.replace('with a total of', ',')
+clip = clip.replace('\n', ',')
+clip = clip.replace('and three', ', three')  # serial comma workaround
+clip = clip.replace('and four', ', four')  # serial comma workaround
+clip = clip.replace('and five', ', five')  # serial comma workaround
+
+def tee(s):  # print to screen and also write to log
+    print(s)
+    with open('shop.log', 'a') as f:
+        f.write(s + '\n')
+
+items = clip.split(',')
+for line in items:
+    if len(line) < 2:
+        pass  # blank line
+    elif line.startswith('Queued'):
+        pass  # meaningless line
+    elif line.endswith(' items.'):
+        pass  # meaningless line
+    else:
+        filtered.append(line.strip())
+        tee(line)
+
+print()
+os.system("PAUSE")
+
+#EOF
